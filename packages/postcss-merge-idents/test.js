b6775b400d1b095b4cc70c798db15c6bb172a3d1
Ben Briggs <beneb.info@gmail.com>
2015-05-10 22:40:47 +0100

    Initial commit.

---
+++ packages/postcss-merge-idents/test.js
@@ -0,0 +1,65 @@
+var test = require('tape');
+var postcss = require('postcss');
+var plugin = require('./');
+var name = require('./package.json').name;
+
+var tests = [{
+    message: 'should merge keyframe identifiers',
+    fixture: '@keyframes a{0%{color:#fff}to{color:#000}}@keyframes b{0%{color:#fff}to{color:#000}}',
+    expected: '@keyframes b{0%{color:#fff}to{color:#000}}',
+}, {
+    message: 'should merge multiple keyframe identifiers',
+    fixture: '@keyframes a{0%{color:#fff}to{color:#000}}@keyframes b{0%{color:#fff}to{color:#000}}@keyframes c{0%{color:#fff}to{color:#000}}',
+    expected: '@keyframes c{0%{color:#fff}to{color:#000}}',
+}, {
+    message: 'should update relevant animation declarations',
+    fixture: '@keyframes a{0%{color:#fff}to{color:#000}}@keyframes b{0%{color:#fff}to{color:#000}}div{animation:a .2s ease}',
+    expected: '@keyframes b{0%{color:#fff}to{color:#000}}div{animation:b .2s ease}'
+}, {
+    message: 'should update relevant animation declarations (2)',
+    fixture: '@keyframes a{0%{color:#fff}to{color:#000}}@keyframes b{0%{color:#fff}to{color:#000}}@keyframes c{0%{color:#fff}to{color:#000}}div{animation:a .2s ease}',
+    expected: '@keyframes c{0%{color:#fff}to{color:#000}}div{animation:c .2s ease}'
+}, {
+    message: 'should not merge vendor prefixed keyframes',
+    fixture: '@-webkit-keyframes a{0%{color:#fff}to{color:#000}}@keyframes a{0%{color:#fff}to{color:#000}}',
+    expected: '@-webkit-keyframes a{0%{color:#fff}to{color:#000}}@keyframes a{0%{color:#fff}to{color:#000}}'
+}, {
+    message: 'should merge counter style identifiers',
+    fixture: '@counter-style a{system:extends decimal;suffix:"> "}@counter-style b{system:extends decimal;suffix:"> "}',
+    expected: '@counter-style b{system:extends decimal;suffix:"> "}',
+}, {
+    message: 'should merge multiple counter style identifiers',
+    fixture: '@counter-style a{system:extends decimal;suffix:"> "}@counter-style b{system:extends decimal;suffix:"> "}@counter-style c{system:extends decimal;suffix:"> "}',
+    expected: '@counter-style c{system:extends decimal;suffix:"> "}',
+}, {
+    message: 'should update relevant list style declarations',
+    fixture: '@counter-style a{system:extends decimal;suffix:"> "}@counter-style b{system:extends decimal;suffix:"> "}ol{list-style:a}',
+    expected: '@counter-style b{system:extends decimal;suffix:"> "}ol{list-style:b}'
+}, {
+    message: 'should update relevant list style declarations (2)',
+    fixture: '@counter-style a{system:extends decimal;suffix:"> "}@counter-style b{system:extends decimal;suffix:"> "}@counter-style c{system:extends decimal;suffix:"> "}ol{list-style:a}',
+    expected: '@counter-style c{system:extends decimal;suffix:"> "}ol{list-style:c}'
+}, {
+    message: 'should update relevant system declarations',
+    fixture: '@counter-style a{system:extends decimal;suffix:"> "}@counter-style b{system:extends a;suffix:"> "}@counter-style c{system:extends a;suffix:"> "}ol{list-style:c}',
+    expected: '@counter-style a{system:extends decimal;suffix:"> "}@counter-style c{system:extends a;suffix:"> "}ol{list-style:c}'
+}];
+
+function process (css, options) {
+    return postcss(plugin(options)).process(css).css;
+}
+
+test(name, function (t) {
+    t.plan(tests.length);
+
+    tests.forEach(function (test) {
+        var options = test.options || {};
+        t.equal(process(test.fixture, options), test.expected, test.message);
+    });
+});
+
+test('should use the postcss plugin api', function (t) {
+    t.plan(2);
+    t.ok(plugin().postcssVersion, 'should be able to access version');
+    t.equal(plugin().postcssPlugin, name, 'should be able to access name');
+});
