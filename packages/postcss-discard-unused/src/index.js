51665a741140c4a5d228966cc29c749067b362fb
Ben Briggs <beneb.info@gmail.com>
2015-08-13 18:15:52 +0100

    Converted the module to ES6.

--- packages/postcss-discard-unused/src/index.js
+++ packages/postcss-discard-unused/src/index.js
@@ -1,19 +1,17 @@
 'use strict';
 
-var uniqs = require('uniqs');
-var postcss = require('postcss');
-var flatten = require('flatten');
-var comma = postcss.list.comma;
-var space = postcss.list.space;
+import uniqs from 'uniqs';
+import postcss, {list} from 'postcss';
+import flatten from 'flatten';
 
-function filterAtRule (css, properties, atrule) {
-    var atRules = [];
-    var values = [];
-    css.eachInside(function (node) {
+let {comma: comma, space: space} = list;
+
+let filterAtRule = (css, properties, atrule) => {
+    let atRules = [];
+    let values = [];
+    css.eachInside(node => {
         if (node.type === 'decl' && properties.test(node.prop)) {
-            return comma(node.value).forEach(function (val) {
-                values.push(space(val));
-            });
+            return comma(node.value).forEach(val => values.push(space(val)));
         }
         if (node.type === 'atrule') {
             if (typeof atrule === 'string' && node.name === atrule) {
@@ -24,22 +22,24 @@ function filterAtRule (css, properties, atrule) {
         }
     });
     values = uniqs(flatten(values));
-    atRules.forEach(function (node) {
-        var hasAtRule = values.some(function (c) {
-            return c === node.params;
-        });
+    atRules.forEach(node => {
+        let hasAtRule = values.some(value => value === node.params);
         if (!hasAtRule) {
             node.removeSelf();
         }
     });
-}
+};
 
-module.exports = postcss.plugin('postcss-discard-unused', function () {
-    return function (css) {
+let hasFont = (fontFamily, cache) => {
+    return comma(fontFamily).some(font => cache.some(c => ~c.indexOf(font)));
+};
+
+module.exports = postcss.plugin('postcss-discard-unused', () => {
+    return css => {
         // fonts have slightly different logic
-        var atRules = [];
-        var values = [];
-        css.eachInside(function (node) {
+        let atRules = [];
+        let values = [];
+        css.eachInside(node => {
             if (node.type === 'decl' &&
                 node.parent.type === 'rule' &&
                 /font(|-family)/.test(node.prop)
@@ -51,21 +51,14 @@ module.exports = postcss.plugin('postcss-discard-unused', function () {
             }
         });
         values = uniqs(flatten(values));
-        atRules.forEach(function (rule) {
-            var fontFamilies = rule.nodes.filter(function (node) {
-                return node.prop === 'font-family';
-            });
+        atRules.forEach(rule => {
+            let families = rule.nodes.filter(node => node.prop === 'font-family');
             // Discard the @font-face if it has no font-family
-            if (!fontFamilies.length) {
+            if (!families.length) {
                 return rule.removeSelf();
             }
-            fontFamilies.forEach(function (family) {
-                var hasFont = comma(family.value).some(function (font) {
-                    return values.some(function (c) {
-                        return ~c.indexOf(font);
-                    });
-                });
-                if (!hasFont) {
+            families.forEach(family => {
+                if (!hasFont(family.value, values)) {
                     rule.removeSelf();
                 }
             });
