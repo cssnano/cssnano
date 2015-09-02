98c936615ce0f4e08ef23c89e1957914a3ba38aa
Bogdan Chadkin <trysound@yandex.ru>
2015-09-02 16:30:43 +0300

    Initial

---
+++ packages/postcss-minify-params/index.js
@@ -0,0 +1,68 @@
+var postcss = require('postcss');
+var parser = require('postcss-value-parser');
+var stringify = parser.stringify;
+var natural = require('javascript-natural-sort');
+var uniqs = require('uniqs');
+
+function trim(nodes) {
+    if (nodes.length) {
+        if (nodes[0].type === 'space') {
+            nodes.shift();
+        }
+    }
+
+    if (nodes.length) {
+        if (nodes[nodes.length - 1].type === 'space') {
+            nodes.pop();
+        }
+    }
+}
+
+function split(nodes, div) {
+    var i, max, node;
+    var result = [];
+    var last = '';
+
+    for (i = 0, max = nodes.length; i < max; i += 1) {
+        node = nodes[i];
+        if (node.type === 'div' && node.value === div) {
+            result.push(last);
+            last = '';
+        } else {
+            last += stringify(node);
+        }
+    }
+
+    result.push(last);
+
+    return result;
+}
+
+module.exports = postcss.plugin('postcss-minify-params', function () {
+    return function (css) {
+        css.walkAtRules(function (rule) {
+            if (!rule.params) {
+                return;
+            }
+
+            var params = parser(rule.params);
+
+            params.walk(function (node) {
+                if (node.type === 'div') {
+                    node.before = '';
+                    node.after = '';
+                }
+
+                if (node.type === 'space') {
+                    node.value = ' ';
+                }
+
+                if (node.type === 'function') {
+                    trim(node.nodes);
+                }
+            }, true);
+
+            rule.params = uniqs(split(params.nodes, ',')).sort(natural).join();
+        });
+    };
+});
---
+++ packages/postcss-minify-params/test.js
@@ -0,0 +1,25 @@
+var postcss = require('postcss');
+var plugin = require('./');
+var test  = require('tape');
+
+var tests = [{
+    message: 'should normalise @media queries',
+    fixture: '@media screen ,\tprint {h1{color:red}}@media print,screen{h2{color:blue}}',
+    expected: '@media print,screen {h1{color:red}}@media print,screen{h2{color:blue}}'
+}, {
+    message: 'should normalise @media queries (2)',
+    fixture: '@media only screen \n and ( min-width: 400px, min-height: 500px ){h1{color:blue}}',
+    expected: '@media only screen and (min-width:400px,min-height:500px){h1{color:blue}}'
+}, {
+    message: 'should not mangle @keyframe from & 100% in other values',
+    fixture: '@keyframes test{x-from-tag{color:red}5100%{color:blue}}',
+    expected: '@keyframes test{x-from-tag{color:red}5100%{color:blue}}'
+}];
+
+test('postcss-minify-params', function (t) {
+    t.plan(tests.length);
+
+    tests.forEach(function (test) {
+        t.equal(postcss(plugin).process(test.fixture).css, test.expected, test.message);
+    });
+});
