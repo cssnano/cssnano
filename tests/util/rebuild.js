ffcf76f407ce1eb80030babc31b23ca55ec0dac9
Ben Briggs <beneb.info@gmail.com>
2015-09-06 13:02:31 +0100

    Update rebuild script.

--- tests/util/rebuild.js
+++ tests/util/rebuild.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var nano = require('../../');
 var directory = require('fs').readdirSync;
 var file = require('fs').readFileSync;
@@ -7,15 +9,16 @@ var formatter = require('./formatter');
 var path = require('path');
 var base = path.join(__dirname, '../integrations');
 
-function formatted (css) {
-    return postcss().use(nano()).use(formatter).process(css).css;
+function formatted (css, cb) {
+    return postcss([ nano(), formatter ]).process(css).then(function (res) {
+        cb(res.css);
+    });
 }
 
 directory(base).forEach(function (f) {
     if (~f.indexOf('fixture')) {
-        write(
-            path.join(base, f.replace('fixture', 'expected')),
-            formatted(file(base + '/' + f))
-        );
+        formatted(file(base + '/' + f), function (res) {
+            write(path.join(base, f.replace('fixture', 'expected')), res);
+        });
     }
 });
