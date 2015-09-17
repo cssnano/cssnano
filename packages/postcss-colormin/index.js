b0160b345fd6af61a0968c73c69bd06a8bef2027
Ben Briggs <beneb.info@gmail.com>
2015-09-17 14:42:15 +0100

    Tidy up index.js.

--- packages/postcss-colormin/index.js
+++ packages/postcss-colormin/index.js
@@ -6,41 +6,43 @@ var parser = require('postcss-value-parser');
 var stringify = parser.stringify;
 var trim = parser.trim;
 
+function optimise (decl) {
+    if (/^(?!font|filter|-webkit-tap-highlight-color)/.test(decl.prop)) {
+        decl.value = parser(decl.value).walk(function (node) {
+            if (node.type === 'function') {
+                if (/^(rgb|hsl)a?$/.test(node.value)) {
+                    node.value = colormin(stringify(node));
+                    node.type = 'word';
+                } else {
+                    trim(node.nodes);
+                }
+            } else if (node.type === 'div') {
+                node.before = '';
+                node.after = '';
+            } else if (node.type === 'space') {
+                node.value = ' ';
+            } else {
+                node.value = colormin(node.value);
+            }
+        }).toString();
+    }
+    if (decl.prop === '-webkit-tap-highlight-color') {
+        if (decl.value === 'inherit' || decl.value === 'transparent') {
+            return;
+        }
+        decl.value = parser(decl.value).walk(function (node) {
+            if (node.type === 'function') {
+                trim(node.nodes);
+            } else if (node.type === 'div') {
+                node.before = '';
+                node.after = '';
+            }
+        }).toString();
+    }
+}
+
 module.exports = postcss.plugin('postcss-colormin', function () {
     return function (css) {
-        css.walkDecls(function (decl) {
-            if (/^(?!font|filter|-webkit-tap-highlight-color)/.test(decl.prop)) {
-                decl.value = parser(decl.value).walk(function (node) {
-                    if (node.type === 'function') {
-                        if (/^(rgb|hsl)a?$/.test(node.value)) {
-                            node.value = colormin(stringify(node));
-                            node.type = 'word';
-                        } else {
-                            trim(node.nodes);
-                        }
-                    } else if (node.type === 'div') {
-                        node.before = '';
-                        node.after = '';
-                    } else if (node.type === 'space') {
-                        node.value = ' ';
-                    } else {
-                        node.value = colormin(node.value);
-                    }
-                }).toString();
-            }
-            if (decl.prop === '-webkit-tap-highlight-color') {
-                if (decl.value === 'inherit' || decl.value === 'transparent') {
-                    return;
-                }
-                decl.value = parser(decl.value).walk(function (node) {
-                    if (node.type === 'function') {
-                        trim(node.nodes);
-                    } else if (node.type === 'div') {
-                        node.before = '';
-                        node.after = '';
-                    }
-                }).toString();
-            }
-        });
+        css.walkDecls(optimise);
     };
 });
