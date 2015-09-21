3037db9000ce1c17f0d729a47029c1eae98399ff
Ben Briggs <beneb.info@gmail.com>
2015-09-21 14:08:25 +0100

    Rewrite leadingStar hack with less iterations.

--- packages/stylehacks/src/plugins/leadingStar.js
+++ packages/stylehacks/src/plugins/leadingStar.js
@@ -5,33 +5,37 @@ import plugin from '../plugin';
 let targets = ['ie 5.5', 'ie 6', 'ie 7'];
 
 export default plugin(targets, function () {
-    this.css.walkDecls(decl => {
-        let before = decl.raws.before;
-        if (!before) {
-            return;
-        }
-        let hacks = '!_$_&_*_)_=_%_+_,_._/_`_]_#_~_?_:_|'.split('_');
-        let hasBefore = hacks.some(hack => {
-            if (~before.indexOf(hack)) {
-                this.push(decl, `Bad property: ${before.trim()}${decl.prop}`);
-                return true;
+    this.css.walk(node => {
+        if (node.type === 'decl') {
+            let before = node.raws.before;
+            if (!before) {
+                return;
             }
-        });
-        if (!hasBefore) {
-            // some values are not picked up by before, so ensure they are
-            // at the beginning of the value
-            hacks.some(hack => {
-                if (!decl.prop.indexOf(hack)) {
-                    this.push(decl, `Bad property: ${decl.prop}`);
+            let hacks = '!_$_&_*_)_=_%_+_,_._/_`_]_#_~_?_:_|'.split('_');
+            let hasBefore = hacks.some(hack => {
+                if (~before.indexOf(hack)) {
+                    this.push(node, `Bad property: ${before.trim()}${node.prop}`);
                     return true;
                 }
             });
+            if (!hasBefore) {
+                // some values are not picked up by before, so ensure they are
+                // at the beginning of the value
+                hacks.some(hack => {
+                    if (!node.prop.indexOf(hack)) {
+                        this.push(node, `Bad property: ${node.prop}`);
+                        return true;
+                    }
+                });
+            }
         }
-    });
-    this.css.walkAtRules(rule => {
-        // test for the @property: value; hack
-        if (rule.name.lastIndexOf(':') === rule.name.length - 1) {
-            this.push(rule, `Bad property: @${rule.name.substr(0, rule.name.length - 1)}`);
+        if (node.type === 'atrule') {
+            // test for the @property: value; hack
+            let name = node.name;
+            let len = name.length - 1;
+            if (name.lastIndexOf(':') === len) {
+                this.push(node, `Bad property: @${name.substr(0, len)}`);
+            }
         }
     });
 });
