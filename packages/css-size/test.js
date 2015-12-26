6b63a6a0e7002e7a7dd12549d32743809b202834
Ben Briggs <beneb.info@gmail.com>
2015-12-26 00:45:09 +0000

    Fixes tests for new ava api.

--- packages/css-size/test.js
+++ packages/css-size/test.js
@@ -6,33 +6,36 @@ var path = require('path');
 var size = require('./');
 var read = require('fs').readFileSync;
 
-function setup (args, callback) {
-    process.chdir(__dirname);
+function setup (args) {
+    return new Promise(function (resolve, reject) {
+        process.chdir(__dirname);
 
-    var ps = spawn(process.execPath, [
-        path.resolve(__dirname, './cli.js')
-    ].concat(args));
+        var ps = spawn(process.execPath, [
+            path.resolve(__dirname, './cli.js')
+        ].concat(args));
 
-    var out = '';
-    var err = '';
+        var out = '';
+        var err = '';
 
-    ps.stdout.on('data', function (buffer) {
-        out += buffer;
-    });
-    ps.stderr.on('data', function (buffer) {
-        err += buffer;
-    });
+        ps.stdout.on('data', function (buffer) {
+            out += buffer;
+        });
+        ps.stderr.on('data', function (buffer) {
+            err += buffer;
+        });
 
-    ps.on('exit', function (code) {
-        callback.call(this, err, out, code);
+        ps.on('exit', function (code) {
+            if (err) {
+                reject(err);
+            }
+            resolve([out, code]);
+        });
     });
 }
 
 test('cli', function (t) {
-    t.plan(5);
-
-    setup(['test.css'], function (err, out) {
-        t.notOk(err);
+    return setup(['test.css']).then(function (results) {
+        var out = results[0];
         t.ok(~out.indexOf('43 B'));
         t.ok(~out.indexOf('34 B'));
         t.ok(~out.indexOf('9 B'));
@@ -41,9 +44,7 @@ test('cli', function (t) {
 });
 
 test('api', function (t) {
-    t.plan(4);
-
-    size(read('test.css', 'utf-8')).then(function (result) {
+    return size(read('test.css', 'utf-8')).then(function (result) {
         t.same(result.original, '43 B');
         t.same(result.minified, '34 B');
         t.same(result.difference, '9 B');
