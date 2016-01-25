3984856e0b1733a3d425d1b83c771ca63d92b291
Ben Briggs <beneb.info@gmail.com>
2016-01-25 11:24:17 +0000

    Reduce AST iterations in the core processor.

--- src/lib/core.js
+++ src/lib/core.js
@@ -1,30 +1,28 @@
 import {plugin} from 'postcss';
 
-function minimiseWhitespace (rule) {
-    rule.raws.before = rule.raws.between = rule.raws.after = '';
-    rule.raws.semicolon = false;
+function minimiseWhitespace (node) {
+    if (node.type === 'decl') {
+        // Ensure that !important values do not have any excess whitespace
+        if (node.important) {
+            node.raws.important = '!important';
+        }
+        // Remove whitespaces around ie 9 hack
+        node.value = node.value.replace(/\s*(\\9)\s*/, '$1');
+        // Remove extra semicolons and whitespace before the declaration
+        if (node.raws.before) {
+            node.raws.before = node.raws.before.replace(/[;\s]/g, '');
+        }
+        node.raws.between = ':';
+        node.raws.semicolon = false;
+    } else if (node.type === 'rule' || node.type === 'atrule') {
+        node.raws.before = node.raws.between = node.raws.after = '';
+        node.raws.semicolon = false;
+    }
 }
 
 export default plugin('cssnano-core', () => {
     return css => {
-        css.walkDecls(decl => {
-            // Ensure that !important values do not have any excess whitespace
-            if (decl.important) {
-                decl.raws.important = '!important';
-            }
-            // Remove whitespaces around ie 9 hack
-            decl.value = decl.value.replace(/\s*(\\9)\s*/, '$1');
-            // Remove extra semicolons and whitespace before the declaration
-            if (decl.raws.before) {
-                decl.raws.before = decl.raws.before.replace(/[;\s]/g, '');
-            }
-            decl.raws.between = ':';
-            decl.raws.semicolon = false;
-        });
-
-        css.walkRules(minimiseWhitespace);
-        css.walkAtRules(minimiseWhitespace);
-
+        css.walk(minimiseWhitespace);
         // Remove final newline
         css.raws.after = '';
     };
