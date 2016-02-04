8beba1da53e46801003d1cc4077297cbfa879749
Ben Briggs <beneb.info@gmail.com>
2016-02-04 12:14:19 +0000

    Add test for warnOnce.

---
+++ src/__tests__/warnOnce.js
@@ -0,0 +1,16 @@
+import warnOnce from '../lib/warnOnce';
+import ava from 'ava';
+import hookStd from 'hook-std';
+
+ava('should output a warning only once', t => {
+    let out = '';
+
+    const unhook = hookStd.stderr({silent: true}, output => out += output);
+    
+    warnOnce('once');
+    warnOnce('once');
+    
+    unhook();
+    
+    t.same(out.trim(), 'once');
+});
