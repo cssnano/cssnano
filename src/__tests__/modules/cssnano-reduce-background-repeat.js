51f711df8a46382299a1d36d8e3379d2ecd5b9c4
Ben Briggs <beneb.info@gmail.com>
2016-05-26 17:45:07 +0100

    Multiple value testing.

--- src/__tests__/modules/cssnano-reduce-background-repeat.js
+++ src/__tests__/modules/cssnano-reduce-background-repeat.js
@@ -22,6 +22,14 @@ Object.keys(mappings).forEach(mapping => {
         message: `should handle ${mapping} (background-repeat)`,
         fixture: `background-repeat:${mappings[mapping]}`,
         expected: `background-repeat:${mapping}`
+    }, {
+        message: `should handle multiple instances (background)`,
+        fixture: `background-repeat:#000 url(cat.jpg) ${mappings[mapping]} 50%,#000 url(cat.jpg) ${mappings[mapping]} 50%`,
+        expected: `background-repeat:#000 url(cat.jpg) ${mapping} 50%,#000 url(cat.jpg) ${mapping} 50%`
+    }, {
+        message: `should handle multiple instances (background-repeat)`,
+        fixture: `background-repeat:${mappings[mapping]},${mappings[mapping]}`,
+        expected: `background-repeat:${mapping},${mapping}`
     });
 });
 
