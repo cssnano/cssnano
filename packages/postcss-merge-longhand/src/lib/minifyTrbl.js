73f7649be81d246951db646e3834b25fe985d2fb
Ben Briggs <beneb.info@gmail.com>
2016-05-29 16:33:46 +0100

    Use parseTrbl, reduce duplication.

--- packages/postcss-merge-longhand/src/lib/minifyTrbl.js
+++ packages/postcss-merge-longhand/src/lib/minifyTrbl.js
@@ -1,13 +1,7 @@
-import {list} from 'postcss';
+import parseTrbl from './parseTrbl';
 
 export default v => {
-    let s = typeof v === 'string' ? list.space(v) : v;
-    let value = [
-        s[0],                // top
-        s[1] || s[0],        // right
-        s[2] || s[0],        // bottom
-        s[3] || s[1] || s[0] // left
-    ];
+    const value = parseTrbl(v);
     if (value[3] === value[1]) {
         value.pop();
         if (value[2] === value[0]) {
