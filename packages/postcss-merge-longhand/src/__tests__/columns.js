7c4a1007441cdf4520dc12327a1822939a91fa47
Ben Briggs <beneb.info@gmail.com>
2016-05-30 18:53:10 +0100

    Refactor columns merging behaviour, fixes some bugs.

--- packages/postcss-merge-longhand/src/__tests__/columns.js
+++ packages/postcss-merge-longhand/src/__tests__/columns.js
@@ -5,7 +5,7 @@ import plugin from '..';
 const suites = [{
     message: 'should merge column values',
     fixture: 'h1{column-width:12em;column-count:auto}',
-    expected: 'h1{columns:12em auto}'
+    expected: 'h1{columns:12em}'
 }, {
     message: 'should minify column values',
     fixture: 'h1{column-width:auto;column-count:auto}',
@@ -13,7 +13,23 @@ const suites = [{
 }, {
     message: 'should merge column-width with columns',
     fixture: 'h1{columns:12em auto;column-width:11em}',
-    expected: 'h1{columns:11em auto}'
+    expected: 'h1{columns:11em}'
+}, {
+    message: 'should merge column width and column count',
+    fixture: 'h1{column-width:6em;column-count:3}',
+    expected: 'h1{columns:6em 3}'
+}, {
+    message: 'should pass through column width',
+    fixture: 'h1{column-width:6em}',
+    expected: 'h1{column-width:6em}'
+}, {
+    message: 'should pass through column count',
+    fixture: 'h1{column-count:3}',
+    expected: 'h1{column-count:3}'
+}, {
+    message: 'should pass through inherit',
+    fixture: 'h1{column-width:inherit;column-count:inherit}',
+    expected: 'h1{column-width:inherit;column-count:inherit}'
 }];
 
 suites.forEach(suite => {
--- packages/postcss-merge-longhand/src/lib/decl/columns.js
+++ packages/postcss-merge-longhand/src/lib/decl/columns.js
@@ -1,51 +1,79 @@
 import {list} from 'postcss';
 import {unit} from 'postcss-value-parser';
+import canMerge from '../canMerge';
+import hasAllProps from '../hasAllProps';
 import insertCloned from '../insertCloned';
 import getLastNode from '../getLastNode';
 import remove from '../remove';
 
-let wc = ['column-width', 'column-count'];
+const wc = ['column-width', 'column-count'];
+const auto = 'auto';
 
-export default {
-    explode: rule => {
-        rule.eachDecl('columns', decl => {
-            let values = list.space(decl.value).sort();
-            if (values.length === 1) {
-                values.push('auto');
+/**
+ * Normalize a columns shorthand definition. Both of the longhand
+ * properties' initial values are 'auto', and as per the spec,
+ * omitted values are set to their initial values. Thus, we can
+ * remove any 'auto' definition when there are two values.
+ *
+ * Specification link: https://www.w3.org/TR/css3-multicol/
+ */
+
+function normalize (values) {
+    if (values[0] === auto) {
+        return values[1];
+    }
+    if (values[1] === auto) {
+        return values[0];
+    }
+    return values.join(' ');
+}
+
+function explode (rule) {
+    rule.walkDecls('columns', decl => {
+        let values = list.space(decl.value);
+        if (values.length === 1) {
+            values.push(auto);
+        }
+
+        values.forEach((value, i) => {
+            let prop = wc[1];
+
+            if (value === auto) {
+                prop = i === 0 ? wc[0] : wc[1];
+            } else if (unit(value).unit) {
+                prop = wc[0];
             }
 
-            values.forEach((value, i) => {
-                let name = wc[1];
-
-                if (value === 'auto') {
-                    name = i === 0 ? wc[0] : wc[1];
-                } else if (unit(value).unit !== '') {
-                    name = wc[0];
-                }
-
-                insertCloned(rule, decl, {
-                    prop: name,
-                    value: value
-                });
+            insertCloned(rule, decl, {
+                prop,
+                value,
             });
-            decl.removeSelf();
         });
-    },
-    merge: rule => {
-        let decls = rule.nodes.filter(node => node.prop && ~wc.indexOf(node.prop));
-        while (decls.length) {
-            let lastNode = decls[decls.length - 1];
-            let props = decls.filter(node => node.important === lastNode.important);
+        decl.remove();
+    });
+}
+
+function merge (rule) {
+    let decls = rule.nodes.filter(({prop}) => prop && ~wc.indexOf(prop));
+    while (decls.length) {
+        let lastNode = decls[decls.length - 1];
+        let props = decls.filter(node => node.important === lastNode.important);
+        if (
+            hasAllProps.apply(this, [props].concat(wc)) &&
+            canMerge.apply(this, props)
+        ) {
             let values = wc.map(prop => getLastNode(props, prop).value);
-            if (values.length > 1 && values[0] === values[1]) {
-                values.pop();
-            }
             insertCloned(rule, lastNode, {
                 prop: 'columns',
-                value: values.join(' ')
+                value: normalize(values),
             });
             props.forEach(remove);
-            decls = decls.filter(node => !~props.indexOf(node));
         }
+        decls = decls.filter(node => !~props.indexOf(node));
     }
+}
+
+export default {
+    explode,
+    merge,
 };
