3e48a8c504f654aaad08f9dc237e70e08151667e
Ben Briggs <beneb.info@gmail.com>
2016-06-06 22:14:08 +0100

    Update integration tests, process no longer hangs on failing cases.

--- src/__tests__/integrations.js
+++ src/__tests__/integrations.js
@@ -3,19 +3,17 @@ import nano from '..';
 import {readFileSync as file} from 'fs';
 import {join} from 'path';
 import postcss from 'postcss';
-import specName from './util/specName';
 import formatter from './util/formatter';
 import frameworks from 'css-frameworks';
 
 const base = join(__dirname, 'integrations');
 
 Object.keys(frameworks).forEach(framework => {
-    const testName = 'produceTheExpectedResultFor: ' + framework + '.css';
     ava(framework + '.css', t => {
         const plugins = [nano(), formatter()];
-        return postcss(plugins).process(frameworks[framework]).then(result => {
+        return postcss(plugins).process(frameworks[framework]).then(({css}) => {
             const expected = file(join(base, framework) + '.css', 'utf-8');
-            t.deepEqual(result.css, expected, specName(testName));
+            t.deepEqual(css, expected);
         });
     });
 });
