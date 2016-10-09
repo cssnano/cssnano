ac8a989844c9807489b5b534d127b091f5ff16ad
Ben Briggs <beneb.info@gmail.com>
2016-10-09 22:41:43 +0100

    Further improve css mixin handling.

--- src/lib/core.js
+++ src/lib/core.js
@@ -1,7 +1,15 @@
 import {plugin} from 'postcss';
 
+const atrule = 'atrule';
+const decl = 'decl';
+const rule = 'rule';
+
 function minimiseWhitespace (node) {
-    if (node.type === 'decl') {
+    const {type} = node;
+    if (~[decl, rule, atrule].indexOf(type) && node.raws.before) {
+        node.raws.before = node.raws.before.replace(/\s/g, '');
+    }
+    if (type === decl) {
         // Ensure that !important values do not have any excess whitespace
         if (node.important) {
             node.raws.important = '!important';
@@ -11,15 +19,14 @@ function minimiseWhitespace (node) {
         // Remove extra semicolons and whitespace before the declaration
         if (node.raws.before) {
             const prev = node.prev();
-            if (prev && prev.type !== 'rule') {
+            if (prev && prev.type !== rule) {
                 node.raws.before = node.raws.before.replace(/;/g, '');
             }
-            node.raws.before = node.raws.before.replace(/\s/g, '');
         }
         node.raws.between = ':';
         node.raws.semicolon = false;
-    } else if (node.type === 'rule' || node.type === 'atrule') {
-        node.raws.before = node.raws.between = node.raws.after = '';
+    } else if (type === rule || type === atrule) {
+        node.raws.between = node.raws.after = '';
         node.raws.semicolon = false;
     }
 }
