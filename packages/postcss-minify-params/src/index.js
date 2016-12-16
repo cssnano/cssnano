84b56eef94bd933942c6222a832bf4272ea73db7
Ben Briggs <beneb.info@gmail.com>
2016-12-16 21:58:59 +0000

    Remove `all and` from media queries.
    As per the specification, it's not necessary to specify
    `@media all and (min-width: 500px) {}` - this can be represented as
    `@media (min-width: 500px) {}` instead.
    
    Closes https://github.com/ben-eb/cssnano/issues/215.

--- packages/postcss-minify-params/src/index.js
+++ packages/postcss-minify-params/src/index.js
@@ -24,6 +24,11 @@ function split(nodes, div) {
     return result;
 }
 
+function removeNode(node) {
+    node.value = '';
+    node.type = 'word';
+}
+
 module.exports = postcss.plugin('postcss-minify-params', () => {
     return css => {
         css.walkAtRules(rule => {
@@ -33,17 +38,35 @@ module.exports = postcss.plugin('postcss-minify-params', () => {
 
             const params = valueParser(rule.params);
 
-            params.walk(node => {
+            params.walk((node, index) => {
                 if (node.type === 'div' || node.type === 'function') {
                     node.before = node.after = '';
                 } else if (node.type === 'space') {
                     node.value = ' ';
+                } else if (node.type === 'word') {
+                    if (node.value === 'all' && rule.name === 'media') {
+                        const nextSpace = params.nodes[index + 1];
+                        const nextWord = params.nodes[index + 2];
+                        const secondSpace = params.nodes[index + 3];
+                        if (nextWord && nextWord.value === 'and') {
+                            removeNode(nextWord);
+                            removeNode(nextSpace);
+                            if (secondSpace) {
+                                removeNode(secondSpace);
+                            }
+                        }
+                        removeNode(node);
+                    }
                 }
             }, true);
 
             rule.params = sort(uniqs(split(params.nodes, ',')), {
                 insensitive: true
             }).join();
+
+            if (!rule.params.length) {
+                rule.raws.afterName = '';
+            }
         });
     };
 });
--- packages/postcss-minify-params/test/index.js
+++ packages/postcss-minify-params/test/index.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const postcss = require('postcss');
-const plugin = require('../');
+const plugin = require('../src');
 
 /* eslint-disable */
 
@@ -12,6 +12,18 @@ const tests = [{
     message: 'should normalise @media queries (2)',
     fixture: '@media only screen \n and ( min-width: 400px, min-height: 500px ){h1{color:blue}}',
     expected: '@media only screen and (min-width:400px,min-height:500px){h1{color:blue}}'
+}, {
+    message: 'should normalise "all" in @media queries',
+    fixture: '@media all{h1{color:blue}}',
+    expected: '@media{h1{color:blue}}',
+}, {
+    message: 'should normalise "all and" in @media queries',
+    fixture: '@media all and (min-width:500px){h1{color:blue}}',
+    expected: '@media (min-width:500px){h1{color:blue}}',
+}, {
+    message: 'should not remove "all" from other at-rules',
+    fixture: '@foo all;',
+    expected: '@foo all;',
 }, {
     message: 'should not mangle @keyframe from & 100% in other values',
     fixture: '@keyframes test{x-from-tag{color:red}5100%{color:blue}}',
