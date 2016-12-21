624b81fee2ba6e0983256f3ad9d63cc78f9a48fb
Chris Walker <chris@thechriswalker.net>
2016-12-21 19:51:53 +0000

    Update `acquire` script to new data source.
    Now uses the MDN Data repository https://github.com/mdn/data
    
    Resolves ben-eb/postcss-reduce-initial#17

--- packages/postcss-reduce-initial/src/acquire.js
+++ packages/postcss-reduce-initial/src/acquire.js
@@ -1,38 +1,32 @@
 import got from 'got';
-import cheerio from 'cheerio';
 import isHtml from 'is-html';
 import plainText from 'html2plaintext';
 
-const initialLength = 7;
-const url = 'https://developer.mozilla.org/en-US/docs/Template:CSSData';
 
-got(url).then(({body}) => {
-    const $ = cheerio.load(body);
+const initialLength = 7; // "initial".length;
+const url = 'https://raw.githubusercontent.com/mdn/data/master/css/properties.json';
 
-    const result = $('#wikiArticle pre').text().replace(/[\r\t]/g, '');
-    const data = JSON.parse(result);
-
-    const initialValues = Object.keys(data.properties).reduce((values, key) => {
-        const property = data.properties[key];
-
-        if (
-            // Ignore complex syntaxes
-            typeof property.initial !== 'string' ||
-            // Ignore display as it has different semantics
-            // depending on the selected element. 
-            key === 'display' ||
-            // Ignore anything that doesn't look like <code></code>
-            !isHtml(property.initial)
-        ) {
+got(url, {json:true})
+    .then(({body}) => Object.keys(body)
+        .reduce((values, key) => {
+            let value;
+            const {initial} = body[key];
+            if (
+                // Ignore complex syntaxes
+                typeof initial === 'string' &&
+                // Ignore display as it has different semantics
+                // depending on the selected element. 
+                key !== 'display' &&
+                // Ignore anything that doesn't look like <code></code>
+                isHtml(initial) &&
+                // Ignore if the word "initial" is shorter
+                (value = plainText(initial)).length < initialLength
+            ) {
+                values[key] = value;
+            }
             return values;
-        }
-
-        const value = plainText(property.initial);
-        if (value.length < initialLength) {
-            values[key] = value;
-        }
-        return values;
-    }, {});
-
-    process.stdout.write(`${JSON.stringify(initialValues, null, 2)}\n`);
-}).catch(error => console.log('errored: ', error.response.body));
+        }, {}))
+    .then(
+        data => process.stdout.write(`${JSON.stringify(data, null, 2)}\n`),
+        error => console.log('errored: ', error.response.body)
+    );
