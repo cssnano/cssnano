30cd948a5bdef8ee12012ece15402e9591381925
Ben Briggs <beneb.info@gmail.com>
2016-12-30 21:58:16 +0000

    Update module tests to reflect changes in postcss-normalize-url.

--- src/__tests__/modules/postcss-normalize-url.js
+++ src/__tests__/modules/postcss-normalize-url.js
@@ -81,21 +81,6 @@ module.exports.tests = [{
     expected: '@namespace islands "http://bar.yandex.ru/ui/islands";',
     options: {discardUnused: {namespace: false}},
 }, {
-    message: 'should optimise @namespace urls (2)',
-    fixture: '@namespace test url("http://bar.com:80/test")',
-    expected: '@namespace test "http://bar.com/test"',
-    options: {discardUnused: {namespace: false}},
-}, {
-    message: 'should optimise @namespace urls (3)',
-    fixture: '@namespace test \'http://test.com/foo/../bar\';',
-    expected: '@namespace test \'http://test.com/bar\';',
-    options: {discardUnused: {namespace: false}, normalizeString: false},
-}, {
-    message: 'should optimise @namespace urls (4)',
-    fixture: '@namespace test url("         http://bar.com:80/test        ");',
-    expected: '@namespace test "http://bar.com/test";',
-    options: {discardUnused: {namespace: false}},
-}, {
     message: 'should not normalize @document urls',
     fixture: '@document url(http://www.w3.org/),url-prefix(http://www.w3.org/Style/){body{font-size:2em}}',
     expected: '@document url(http://www.w3.org/),url-prefix(http://www.w3.org/Style/){body{font-size:2em}}',
