8087bc621f42e355d55c9ddff2a1cb47698a7b0b
Ben Briggs <beneb.info@gmail.com>
2017-01-06 18:31:11 +0000

    Rewrote reduce-positions tests and drop modules.js index.

--- src/__tests__/cssnano-reduce-positions.js
+++ src/__tests__/cssnano-reduce-positions.js
@@ -1,6 +1,5 @@
-module.exports.name = 'cssnano/reduce-positions';
-
-let tests = module.exports.tests = [];
+import test from 'ava';
+import processCss from './_processCss';
 
 const directions = ['top', 'right', 'bottom', 'left', 'center'];
 const horizontal = {
@@ -16,41 +15,25 @@ const vertical = {
 const hkeys = Object.keys(horizontal);
 const vkeys = Object.keys(vertical);
 
-const decls = [{
-    property: 'background:',
-    additional: 'url(http://example.com/testing/test.png) no-repeat ',
-    tail: ' #fff',
-}, {
-    property: 'background-position:',
-    additional: '',
-    tail: '',
-}, {
-    property: 'background:',
-    additional: '#000 url(cat.jpg) ',
-    tail: '',
-}, {
-    property: 'perspective-origin:',
-    additional: '',
-    tail: '',
-}, {
-    property: '-webkit-perspective-origin:',
-    additional: '',
-    tail: '',
-}];
-
-decls.forEach(({additional, property, tail}) => {
-    const push = ({message, fixture, expected}) => {
-        tests.push({
-            message: message,
-            fixture: `${property}${additional}` + fixture + tail,
-            expected: `${property}${additional}` + expected + tail,
-        });
-        tests.push({
-            message: message + ' (with multiple arguments)',
-            fixture: `${property}${additional}${fixture}${tail}, ${fixture}${tail}`,
-            expected: `${property}${additional}${expected}${tail},${expected}${tail}`,
+function suite (t, property, additional = '', tail = '') {
+    const tests = [];
+    const push = (...examples) => {
+        examples.forEach(({fixture, expected}) => {
+            tests.push(
+                processCss(
+                    t,
+                    `${property}${additional}${fixture}${tail}`,
+                    `${property}${additional}${expected}${tail}}`
+                ),
+                processCss(
+                    t,
+                    `${property}${additional}${fixture}${tail}, ${fixture}${tail}`,
+                    `${property}${additional}${expected}${tail},${expected}${tail}`
+                ),
+            );
         });
     };
+
     if (property === 'background:') {
         push({
             message: 'should convert <percentage> center/50% 50% to <percentage>/50% 50%',
@@ -62,8 +45,7 @@ decls.forEach(({additional, property, tail}) => {
         message: 'should convert <percentage> center to <percentage>',
         fixture: `30% center`,
         expected: `30%`,
-    });
-    push({
+    }, {
         message: 'should not convert <percentage> <percentage>',
         fixture: `45% 60%`,
         expected: `45% 60%`,
@@ -113,8 +95,7 @@ decls.forEach(({additional, property, tail}) => {
                 message: `should convert "${direction} ${other}" to "${result}"`,
                 fixture: `${direction} ${other}`,
                 expected: `${result}`,
-            });
-            push({
+            }, {
                 message: `should not convert the three value syntax "${direction} ${other} 60px"`,
                 fixture: `${direction} ${other} 60px`,
                 expected: `${direction} ${other} 60px`,
@@ -128,4 +109,35 @@ decls.forEach(({additional, property, tail}) => {
             }
         });
     });
-});
+
+    return Promise.all(tests);
+}
+
+test(
+    suite,
+    'background:',
+    'url(http://example.com/testing/test.png) no-repeat ',
+    ' #fff'
+);
+
+test(
+    suite,
+    'background-position:'
+);
+
+test(
+    suite,
+    'background:',
+    '#000 url(cat.jpg) '
+);
+
+test(
+    suite,
+    'perspective-origin:'
+);
+
+test(
+    suite,
+    '-webkit-perspective-origin:'
+);
+
