af27d14332b810ab9661605f9847454081f5fcba
Ben Briggs <beneb.info@gmail.com>
2017-02-22 19:55:51 +0000

    Also update trailingSlashComma.

--- packages/stylehacks/src/plugins/trailingSlashComma.js
+++ packages/stylehacks/src/plugins/trailingSlashComma.js
@@ -1,15 +1,14 @@
 import plugin from '../plugin';
+import isMixin from '../isMixin';
 import {IE_5_5, IE_6, IE_7} from '../dictionary/browsers';
 import {SELECTOR} from '../dictionary/identifiers';
 import {RULE} from '../dictionary/postcss';
 
 export default plugin([IE_5_5, IE_6, IE_7], [RULE], function (rule) {
-    const {selector} = rule;
-    // If the selector ends with a ':' it is likely a part of a custom mixin,
-    // so just pass through.
-    if (!selector || selector[selector.length - 1] === ':') {
+    if (isMixin(rule)) {
         return;
     }
+    const {selector} = rule;
     const trim = selector.trim();
     if (
         trim.lastIndexOf(',')  === selector.length - 1 ||
