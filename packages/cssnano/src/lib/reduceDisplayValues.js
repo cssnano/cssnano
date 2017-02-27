a351837553808b12b52cfd3ac32e83e60a48c787
Ben Briggs <beneb.info@gmail.com>
2017-02-27 23:38:31 +0000

    Optimise keyword usage for reduceDisplayValues.

--- packages/cssnano/src/lib/reduceDisplayValues.js
+++ packages/cssnano/src/lib/reduceDisplayValues.js
@@ -3,29 +3,50 @@ import valueParser from 'postcss-value-parser';
 import evenValues from './evenValues';
 import getMatchFactory from './getMatch';
 
+const block = 'block';
+const flex = 'flex';
+const flow = 'flow';
+const flowRoot = 'flow-root';
+const grid = 'grid';
+const inline = 'inline';
+const inlineBlock = 'inline-block';
+const inlineFlex = 'inline-flex';
+const inlineGrid = 'inline-grid';
+const inlineListItem = 'inline-list-item';
+const inlineTable = 'inline-table';
+const listItem = 'list-item';
+const ruby = 'ruby';
+const rubyBase = 'ruby-base';
+const rubyText = 'ruby-text';
+const runIn = 'run-in';
+const table = 'table';
+const tableCell = 'table-cell';
+const tableCaption = 'table-caption';
+
+
 /**
  * Specification: https://drafts.csswg.org/css-display/#the-display-properties
  */
 
 const mappings = [
-    ['block', ['block', 'flow']],
-    ['flow-root', ['block', 'flow-root']],
-    ['inline', ['inline', 'flow']],
-    ['inline-block', ['inline', 'flow-root']],
-    ['run-in', ['run-in', 'flow']],
-    ['list-item', ['list-item', 'block', 'flow']],
-    ['inline-list-item', ['list-item', 'inline', 'flow']],
-    ['flex', ['block', 'flex']],
-    ['inline-flex', ['inline', 'flex']],
-    ['grid', ['block', 'grid']],
-    ['inline-grid', ['inline', 'grid']],
-    ['ruby', ['inline', 'ruby']],
-    ['table', ['block', 'table']],
-    ['inline-table', ['inline', 'table']],
-    ['table-cell', ['table-cell', 'flow']],
-    ['table-caption', ['table-caption', 'flow']],
-    ['ruby-base', ['ruby-base', 'flow']],
-    ['ruby-text', ['ruby-text', 'flow']],
+    [block, [block, flow]],
+    [flowRoot, [block, flowRoot]],
+    [inline, [inline, flow]],
+    [inlineBlock, [inline, flowRoot]],
+    [runIn, [runIn, flow]],
+    [listItem, [listItem, block, flow]],
+    [inlineListItem, [listItem, inline, flow]],
+    [flex, [block, flex]],
+    [inlineFlex, [inline, flex]],
+    [grid, [block, grid]],
+    [inlineGrid, [inline, grid]],
+    [ruby, [inline, ruby]],
+    [table, [block, table]],
+    [inlineTable, [inline, table]],
+    [tableCell, [tableCell, flow]],
+    [tableCaption, [tableCaption, flow]],
+    [rubyBase, [rubyBase, flow]],
+    [rubyText, [rubyText, flow]],
 ];
 
 const getMatch = getMatchFactory(mappings);
