7a89d692fff3cf95644905d907b52c67577fb1ff
Ben Briggs <beneb.info@gmail.com>
2017-03-02 18:55:02 +0000

    Add bulk package.json update script.

---
+++ util/updatePackages.js
@@ -0,0 +1,29 @@
+import {basename, join} from 'path';
+import glob from 'glob';
+import writeFile from 'write-file';
+
+const homepage = `https://github.com/ben-eb/cssnano`;
+
+function updatePackage (pkg) {
+    const pkgName = basename(pkg);
+    const pkgJson = require(`${pkg}/package.json`);
+
+    pkgJson.repository = `${homepage}/tree/master/packages/${pkgName}`;
+    pkgJson.homepage = homepage;
+
+    pkgJson.bugs = pkgJson.bugs || {};
+    pkgJson.bugs.url = `${homepage}/issues`;
+
+    writeFile(`${pkg}/package.json`, `${JSON.stringify(pkgJson, null, 2)}\n`, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+glob(`${join(__dirname, '../packages')}/*`, (err, packages) => {
+    if (err) {
+        throw err;
+    }
+    packages.forEach(updatePackage);
+});
