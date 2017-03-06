594dbe0bc0d387445672236c3fe4f43fdbd0e20b
Ben Briggs <beneb.info@gmail.com>
2017-03-06 13:15:55 +0000

    Ensure package versions are consistent with each other.

--- util/updatePackages.js
+++ util/updatePackages.js
@@ -4,6 +4,18 @@ import writeFile from 'write-file';
 
 const homepage = `https://github.com/ben-eb/cssnano`;
 
+function semverMajor (dependencies) {
+    Object.keys(dependencies).forEach(dependency => {
+        let version = dependencies[dependency];
+        if (version[0] === '^' && version[1] !== '0') {
+            version = version.split('.')[0] + '.0.0';
+        }
+        dependencies[dependency] = version;
+    });
+
+    return dependencies;
+}
+
 function updatePackage (pkg) {
     const pkgName = basename(pkg);
     const pkgJson = require(`${pkg}/package.json`);
@@ -14,6 +26,14 @@ function updatePackage (pkg) {
 
     pkgJson.bugs = pkgJson.bugs || {};
     pkgJson.bugs.url = `${homepage}/issues`;
+
+    if (pkgJson.dependencies) {
+        pkgJson.dependencies = semverMajor(pkgJson.dependencies);
+    }
+
+    if (pkgJson.devDependencies) {
+        pkgJson.devDependencies = semverMajor(pkgJson.devDependencies);
+    }
 
     writeFile(`${pkg}/package.json`, `${JSON.stringify(pkgJson, null, 2)}\n`, (err) => {
         if (err) {
