bbec1adc29bdc6d916b171eaf7a63be4ed6eb780
Ben Briggs <beneb.info@gmail.com>
2017-03-17 15:04:38 +0000

    Move autoprefixer tests to the advanced preset.

---
+++ packages/cssnano-preset-advanced/src/__tests__/autoprefixer.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import preset from '..';
+import {processCSSWithPresetFactory} from '../../../../util/testHelpers';
+
+const {processCSS} = processCSSWithPresetFactory(preset);
+
+test(
+    'should remove outdated vendor prefixes',
+    processCSS,
+    'h1{-webkit-box-sizing:content-box;box-sizing:content-box}',
+    'h1{box-sizing:content-box}'
+);
+
+const {passthroughCSS} = processCSSWithPresetFactory(preset({
+    autoprefixer: {env: 'safari5'},
+}));
+
+test(
+    'should not remove outdated vendor prefixes when minifying for older browsers',
+    passthroughCSS,
+    'h1{-webkit-border-radius:5px;border-radius:5px}'
+);
+
+const {passthroughCSS: exclude} = processCSSWithPresetFactory(preset({
+    autoprefixer: {exclude: true},
+}));
+
+test(
+    'should not remove outdated vendor prefixes if excluded',
+    exclude,
+    'h1{-webkit-box-sizing:content-box;box-sizing:content-box}'
+);
