3d356db85677512774a8d5ba3f2529dccd036320
Ben Briggs <beneb.info@gmail.com>
2017-05-12 16:56:35 +0100

    Update tests for postcss-calc.

--- packages/postcss-normalize-whitespace/src/__tests__/index.js
+++ packages/postcss-normalize-whitespace/src/__tests__/index.js
@@ -11,13 +11,13 @@ const {processCSS: withDefaultPreset} = processCSSWithPresetFactory('default');
 test(
     'should trim whitespace from nested functions',
     processCSS,
-    'h1{width:calc(10px - ( 100px / 2em ))}',
-    'h1{width:calc(10px - (100px / 2em))}',
+    'h1{width:calc(10px - ( 100px / var(--test) ))}',
+    'h1{width:calc(10px - (100px / var(--test)))}'
 );
 
 test(
     'should trim whitespace from nested functions (preset)',
     withDefaultPreset,
-    'h1{width:calc(10px - ( 100px / 2em ))}',
-    'h1{width:calc(10px - (100px / 2em))}',
+    'h1{width:calc(10px - ( 100px / var(--test) ))}',
+    'h1{width:calc(10px - 100px / var(--test))}',
 );
