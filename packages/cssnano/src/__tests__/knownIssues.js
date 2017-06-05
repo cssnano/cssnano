fe1aa356145c12909368e2489bc739cb2fbe3a24
Ben Briggs <beneb.info@gmail.com>
2017-06-05 13:53:15 +0100

    Add a test case for https://github.com/ben-eb/cssnano/issues/322.

--- packages/cssnano/src/__tests__/knownIssues.js
+++ packages/cssnano/src/__tests__/knownIssues.js
@@ -15,3 +15,20 @@ test.failing(
     `body { padding: 50px; } body { padding: 0; }`,
     `body{padding:0}`
 );
+
+/*
+ * This needs investigation; the property string is truncated
+ * likely because of raw value handling. We might potentially
+ * be able to update this because PostCSS 6 changed how raw
+ * values are handled; however this is a low priority because
+ * CSS is generally not written this way.
+ *
+ * Ref: https://github.com/ben-eb/cssnano/issues/322
+ */
+
+test.failing(
+    'should correctly handle escaped css',
+    processCss,
+    `\\64 \\69 \\76 { \\63 \\6f \\6c \\6f \\72 : \\72 \\67 \\62 \\61 \\28 \\32 \\35 \\35 \\2c \\30 \\2c \\30 \\2c \\2e \\37 \\35 \\29 }`,
+    `\\64\\69\\76{\\63\\6f\\6c\\6f\\72:\\72\\67\\62\\61\\28\\32\\35\\35\\2c\\30\\2c\\30\\2c\\2e\\37\\35\\29}`,
+);
