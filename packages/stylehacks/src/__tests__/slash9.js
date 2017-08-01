d92c6de34dada84057da889bf1c5cad09d2b673b
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2017-08-01 20:36:28 +0300

    Feature(stylehacks): add new hack.

--- packages/stylehacks/src/__tests__/slash9.js
+++ packages/stylehacks/src/__tests__/slash9.js
@@ -8,3 +8,11 @@ test(
     'h1 { }',
     {target: 'ie8', unaffected: 'chrome58'}
 );
+
+test(
+    'ie 6 underscore hack',
+    processCSS,
+    'h1 { margin-top/*\\**/: 1px\\9; }',
+    'h1 { }',
+    {target: 'ie8', unaffected: 'chrome58'}
+);
--- packages/stylehacks/src/plugins/index.js
+++ packages/stylehacks/src/plugins/index.js
@@ -5,6 +5,7 @@ import important from './important';
 import leadingStar from './leadingStar';
 import leadingUnderscore from './leadingUnderscore';
 import mediaSlash0 from './mediaSlash0';
+import mediaSlash0Slash9 from './mediaSlash0Slash9';
 import mediaSlash9 from './mediaSlash9';
 import slash9 from './slash9';
 import starHtml from './starHtml';
@@ -18,6 +19,7 @@ export default [
     leadingStar,
     leadingUnderscore,
     mediaSlash0,
+    mediaSlash0Slash9,
     mediaSlash9,
     slash9,
     starHtml,
