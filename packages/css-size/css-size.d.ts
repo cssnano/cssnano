357cfd511368df5220a26ab8f1bc9ecde759c961
Chris Eppstein <chris@eppsteins.net>
2017-11-24 20:28:58 -0800

    feat(css-size): Add typescript type declarations.

---
+++ packages/css-size/css-size.d.ts
@@ -0,0 +1,49 @@
+export = cssSize;
+
+
+declare function cssSize(
+  css: string,
+  options: cssSize.ProcessOptions,
+  processor?: cssSize.Processor
+): Promise<cssSize.Result<string>>;
+
+declare namespace cssSize {
+  function table(
+    css: string,
+    options: ProcessOptions,
+    processor?: Processor
+  ): Promise<string>;
+
+  function numeric(
+    css: string,
+    options: ProcessOptions,
+    processor?: Processor
+  ): Promise<Result<number>>;
+
+  interface HasCss {
+    css: string;
+  }
+  interface ProcessOptions {
+    [opt: string]: any;
+  }
+  type Processor = (css: string, options: ProcessOptions) => Promise<HasCss>;
+
+  /**
+   * The size before and after, the absolute different and the percent improvement.
+   */
+  interface SizeInfo<T extends string | number> {
+    original: T;
+    processed: T;
+    difference: T;
+    percent: T;
+  }
+
+  /**
+   *  Size deltas of the css in various formats.
+   */
+  interface Result<T extends string | number> {
+    uncompressed: SizeInfo<T>;
+    gzip: SizeInfo<T>;
+    brotli: SizeInfo<T>;
+  }
+}
