841e68a346d1c1e9c8ac937d8f2df6d093a3f837
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-04-19 21:18:20 +0300

    fix: handle uppercase display values and properties

--- packages/postcss-normalize-display-values/src/index.js
+++ packages/postcss-normalize-display-values/src/index.js
@@ -10,16 +10,18 @@ function evenValues (list, index) {
 }
 
 function transform (node) {
+    node.prop = node.prop.toLowerCase();
+
     const {nodes} = valueParser(node.value);
     if (nodes.length === 1) {
         return;
     }
-    const match = getMatch(nodes.filter(evenValues).map(n => n.value));
+    const match = getMatch(nodes.filter(evenValues).map(n => n.value.toLowerCase()));
     if (match) {
         node.value = match;
     }
 }
 
 export default postcss.plugin('postcss-normalize-display-values', () => {
-    return css => css.walkDecls('display', transform);
+    return css => css.walkDecls(/display/i, transform);
 });
