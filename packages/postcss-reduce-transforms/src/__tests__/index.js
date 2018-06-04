ba0dff752fc77558092453938d12f928881f426d
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-06-04 22:12:15 +0300

    fix: case insensitivity fix for `postcss-reduce-transforms`

--- packages/postcss-reduce-transforms/src/__tests__/index.js
+++ packages/postcss-reduce-transforms/src/__tests__/index.js
@@ -12,6 +12,20 @@ test(
 );
 
 test(
+    'should shorten matrix3d(a, b, 0, 0, c, d, 0, 0, 0, 0, 1, 0, tx, ty, 0, 1) to matrix(a, b, c, d, tx, ty) (uppercase property)',
+    processCSS,
+    'h1{TRANSFORM:matrix3d(20, 20, 0, 0, 40, 40, 0, 0, 0, 0, 1, 0, 80, 80, 0, 1)}',
+    'h1{TRANSFORM:matrix(20, 20, 40, 40, 80, 80)}'
+);
+
+test(
+    'should shorten matrix3d(a, b, 0, 0, c, d, 0, 0, 0, 0, 1, 0, tx, ty, 0, 1) to matrix(a, b, c, d, tx, ty) (uppercase value)',
+    processCSS,
+    'h1{TRANSFORM:MATRIX3D(20, 20, 0, 0, 40, 40, 0, 0, 0, 0, 1, 0, 80, 80, 0, 1)}',
+    'h1{TRANSFORM:matrix(20, 20, 40, 40, 80, 80)}'
+);
+
+test(
     'should pass through other 3d matrices',
     passthroughCSS,
     'h1{transform:matrix3d(.8535533905932737,.4999999999999999,.14644660940672619,0,-.4999999999999999,.7071067811865476,.4999999999999999,0,.14644660940672619,-.4999999999999999,.8535533905932737,0,22.62994231491119,-20.3223304703363,101.3700576850888,1)}'
@@ -25,10 +39,24 @@ test(
 );
 
 test(
+    'should shorten rotateZ to rotate (uppercase value)',
+    processCSS,
+    'h1{transform:ROTATEZ(180deg)}',
+    'h1{transform:rotate(180deg)}'
+);
+
+test(
      'should shorten rotate3d(1, 0, 0, a) to rotateX(a)',
      processCSS,
      'h1{transform:rotate3d(1, 0, 0, 20deg)}',
      'h1{transform:rotateX(20deg)}'
+);
+
+test(
+    'should shorten rotate3d(1, 0, 0, a) to rotateX(a) (uppercase value)',
+    processCSS,
+    'h1{transform:ROTATE3D(1, 0, 0, 20deg)}',
+    'h1{transform:rotateX(20deg)}'
 );
 
 test(
@@ -170,6 +198,20 @@ test(
     processCSS,
     'h1{-webkit-transform:translate3d(0, 0, 0)}',
     'h1{-webkit-transform:translateZ(0)}'
+);
+
+test(
+    'should work with vendor prefixes (uppercase property)',
+    processCSS,
+    'h1{-WEBKIT-TRANSFORM:translate3d(0, 0, 0)}',
+    'h1{-WEBKIT-TRANSFORM:translateZ(0)}'
+);
+
+test(
+    'should work with vendor prefixes (uppercase value)',
+    processCSS,
+    'h1{-WEBKIT-TRANSFORM:TRANSLATE3D(0, 0, 0)}',
+    'h1{-WEBKIT-TRANSFORM:translateZ(0)}'
 );
 
 test(
