54f19537600bd727d0de5a642b1e81a99224644a
Igor Uvarov <kuflash@ya.ru>
2018-06-06 13:01:32 +0300

    tests: add test for checking compatibility with postcss-font-magician (#504)
    * add test for checking issue #420
    
    * upgrade postcss-font-magician to 2.2.0
    
    * fix version rule for postcss-font-magician
    
    * fix yarn.lock

---
+++ packages/cssnano/src/__tests__/issue420.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+import postcss from 'postcss';
+import fontMagician from 'postcss-font-magician';
+import cssnano from '..';
+
+test('should work with postcss-font-magician with `display` parameter', t => {
+    const css = `
+    body {
+    	font-family: "Alice";
+    }
+    `;
+    return postcss([fontMagician({display: "optional"}), cssnano]).process(css).then(result => {
+        t.deepEqual(result.css, `@font-face{font-display:optional;font-family:Alice;font-style:normal;font-weight:400;src:local("Alice Regular"),local(Alice-Regular),url(//fonts.gstatic.com/s/alice/v9/sZyKh5NKrCk1xkCk_F1S8A.eot#) format("eot"),url(//fonts.gstatic.com/s/alice/v9/l5RFQT5MQiajQkFxjDLySg.woff2) format("woff2"),url(//fonts.gstatic.com/s/alice/v9/_H4kMcdhHr0B8RDaQcqpTA.woff) format("woff")}body{font-family:Alice}`);
+    });
+});
