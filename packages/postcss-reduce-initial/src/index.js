6f1210368602da1fa70a8289dd2846ae4bbaca94
Andrey Sitnik <andrey@sitnik.ru>
2018-07-05 13:02:11 -0400

    fix: fix caniuse-api browserslist conflict

--- packages/postcss-reduce-initial/src/index.js
+++ packages/postcss-reduce-initial/src/index.js
@@ -1,6 +1,5 @@
 import {plugin} from 'postcss';
 import has from 'has';
-import browserslist from 'browserslist';
 import {isSupported} from 'caniuse-api';
 import fromInitial from '../data/fromInitial.json';
 import toInitial from '../data/toInitial.json';
@@ -10,12 +9,7 @@ const initial = 'initial';
 export default plugin('postcss-reduce-initial', () => {
     return (css, result) => {
         const resultOpts = result.opts || {};
-        const browsers = browserslist(null, {
-            stats: resultOpts.stats,
-            path: __dirname,
-            env: resultOpts.env,
-        });
-        const initialSupport = isSupported('css-initial-value', browsers);
+        const initialSupport = isSupported('css-initial-value');
         css.walkDecls(decl => {
             const lowerCasedProp = decl.prop.toLowerCase();
             if (
