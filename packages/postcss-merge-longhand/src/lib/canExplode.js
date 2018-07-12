62de3eaaf5f6ce9326487bae2ccbb23c22b1522e
Andy Jansson <jansson.andy@gmail.com>
2018-07-12 15:12:26 +0200

    Fix linting error

--- packages/postcss-merge-longhand/src/lib/canExplode.js
+++ packages/postcss-merge-longhand/src/lib/canExplode.js
@@ -3,4 +3,4 @@ import isCustomProp from './isCustomProp';
 const isInherit = node => ~node.value.indexOf('inherit');
 const isInitial = node => ~node.value.indexOf('initial');
 
-export default prop => !isInherit(prop) && !isInitial(prop) && !isCustomProp(prop)
+export default prop => !isInherit(prop) && !isInitial(prop) && !isCustomProp(prop);
