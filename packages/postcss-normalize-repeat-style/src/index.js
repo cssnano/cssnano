7d29b14649a7d8dc9c54251ae0b704a20196a748
evilebottnawi <sheo13666q@gmail.com>
2019-01-30 14:35:35 +0300

    perf(postcss-normalize-repeat-style): increase perf

--- packages/postcss-normalize-repeat-style/src/index.js
+++ packages/postcss-normalize-repeat-style/src/index.js
@@ -12,55 +12,89 @@ const repeatKeywords = mappings.map((mapping) => mapping[0]);
 
 const getMatch = getMatchFactory(mappings);
 
-function transform (decl) {
-    const values = valueParser(decl.value);
-    if (values.nodes.length === 1) {
-        return;
-    }
-    const args = getArguments(values);
-    const relevant = [];
-    args.forEach(arg => {
-        relevant.push({
-            start: null,
-            end: null,
-        });
-        arg.forEach((part, index) => {
-            const isRepeat = ~repeatKeywords.indexOf(part.value.toLowerCase());
-            const len = relevant.length - 1;
-            if (relevant[len].start === null && isRepeat) {
-                relevant[len].start = index;
-                relevant[len].end = index;
+export default postcss.plugin('postcss-normalize-repeat-style', () => {
+    return css => {
+        const cache = {};
+
+        css.walkDecls(/background(-repeat)?|(-webkit-)?mask-repeat/i, (decl) => {
+            const value = decl.value;
+
+            if (cache[value]) {
+                decl.value = cache[value];
+
                 return;
             }
-            if (relevant[len].start !== null) {
-                if (part.type === 'space') {
-                    return;
-                } else if (isRepeat) {
-                    relevant[len].end = index;
+
+            const parsed = valueParser(value);
+
+            if (parsed.nodes.length === 1) {
+                cache[value] = value;
+
+                return;
+            }
+
+            const args = getArguments(parsed);
+            const relevant = [];
+
+            args.forEach(arg => {
+                relevant.push({
+                    start: null,
+                    end: null,
+                });
+
+                arg.forEach((part, index) => {
+                    const isRepeat = ~repeatKeywords.indexOf(
+                        part.value.toLowerCase()
+                    );
+                    const len = relevant.length - 1;
+
+                    if (relevant[len].start === null && isRepeat) {
+                        relevant[len].start = index;
+                        relevant[len].end = index;
+
+                        return;
+                    }
+
+                    if (relevant[len].start !== null) {
+                        if (part.type === 'space') {
+                            return;
+                        } else if (isRepeat) {
+                            relevant[len].end = index;
+
+                            return;
+                        }
+
+                        return;
+                    }
+                });
+            });
+
+            relevant.forEach((range, index) => {
+                if (range.start === null) {
                     return;
                 }
-                return;
-            }
-        });
-    });
-    relevant.forEach((range, index) => {
-        if (range.start === null) {
-            return;
-        }
-        const val = args[index].slice(range.start, range.end + 1);
-        if (val.length !== 3) {
-            return;
-        }
-        const match = getMatch(val.filter(evenValues).map(n => n.value.toLowerCase()));
-        if (match) {
-            args[index][range.start].value = match;
-            args[index][range.start + 1].value = '';
-            args[index][range.end].value = '';
-        }
-    });
-    decl.value = values.toString();
-}
 
-export default postcss.plugin('postcss-normalize-repeat-style', () => {
-    return css => css.walkDecls(/background(-repeat)?|(-webkit-)?mask-repeat/i, transform);
+                const val = args[index].slice(range.start, range.end + 1);
+
+                if (val.length !== 3) {
+                    return;
+                }
+
+                const match = getMatch(
+                    val.filter(evenValues).map(n => n.value.toLowerCase())
+                );
+
+                if (match) {
+                    args[index][range.start].value = match;
+                    args[index][range.start + 1].value = '';
+                    args[index][range.end].value = '';
+                }
+            });
+
+            const result = parsed.toString();
+
+            decl.value = result;
+            cache[value] = result;
+        });
+    };
 });
