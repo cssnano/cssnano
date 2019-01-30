84294e97da82bd2fb5cf9299f0a9dc4441c8d70c
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-01-30 19:03:55 +0300

    perf(postcss-discard-comments): increase perf (#700)

--- packages/postcss-discard-comments/src/index.js
+++ packages/postcss-discard-comments/src/index.js
@@ -1,35 +1,62 @@
-import {plugin, list} from 'postcss';
-import CommentRemover from './lib/commentRemover';
-import commentParser from './lib/commentParser';
+import {plugin, list} from "postcss";
+import CommentRemover from "./lib/commentRemover";
+import commentParser from "./lib/commentParser";
 
 const {space} = list;
 
-export default plugin('postcss-discard-comments', (opts = {}) => {
+export default plugin("postcss-discard-comments", (opts = {}) => {
     const remover = new CommentRemover(opts);
+    const matcherCache = {};
+    const replacerCache = {};
 
     function matchesComments (source) {
-        return commentParser(source).filter(([type]) => type);
+        if (matcherCache[source]) {
+            return matcherCache[source];
+        }
+
+        const result = commentParser(source).filter(([type]) => type);
+
+        matcherCache[source] = result;
+
+        return result;
     }
 
-    function replaceComments (source, separator = ' ') {
-        const parsed = commentParser(source).reduce((value, [type, start, end]) => {
-            const contents = source.slice(start, end);
-            if (!type) {
-                return value + contents;
-            }
-            if (remover.canRemove(contents)) {
-                return value + separator;
-            }
-            return `${value}/*${contents}*/`;
-        }, '');
+    function replaceComments (source, separator = " ") {
+        const key = source + "@|@" + separator;
 
-        return space(parsed).join(' ');
+        if (replacerCache[key]) {
+            return replacerCache[key];
+        }
+
+        const parsed = commentParser(source).reduce(
+            (value, [type, start, end]) => {
+                const contents = source.slice(start, end);
+
+                if (!type) {
+                    return value + contents;
+                }
+
+                if (remover.canRemove(contents)) {
+                    return value + separator;
+                }
+
+                return `${value}/*${contents}*/`;
+            },
+            ""
+        );
+
+        const result = space(parsed).join(" ");
+
+        replacerCache[key] = result;
+
+        return result;
     }
 
     return css => {
         css.walk(node => {
-            if (node.type === 'comment' && remover.canRemove(node.text)) {
+            if (node.type === "comment" && remover.canRemove(node.text)) {
                 node.remove();
+
                 return;
             }
 
@@ -37,39 +64,60 @@ export default plugin('postcss-discard-comments', (opts = {}) => {
                 node.raws.between = replaceComments(node.raws.between);
             }
 
-            if (node.type === 'decl') {
+            if (node.type === "decl") {
                 if (node.raws.value && node.raws.value.raw) {
                     if (node.raws.value.value === node.value) {
                         node.value = replaceComments(node.raws.value.raw);
                     } else {
                         node.value = replaceComments(node.value);
                     }
+
                     node.raws.value = null;
                 }
+
                 if (node.raws.important) {
                     node.raws.important = replaceComments(node.raws.important);
+
                     const b = matchesComments(node.raws.important);
-                    node.raws.important = b.length ? node.raws.important : '!important';
+
+                    node.raws.important = b.length
+                        ? node.raws.important
+                        : "!important";
                 }
+
                 return;
             }
 
-            if (node.type === 'rule' && node.raws.selector && node.raws.selector.raw) {
-                node.raws.selector.raw = replaceComments(node.raws.selector.raw, '');
+            if (
+                node.type === "rule" &&
+                node.raws.selector &&
+                node.raws.selector.raw
+            ) {
+                node.raws.selector.raw = replaceComments(
+                    node.raws.selector.raw,
+                    ""
+                );
+
                 return;
             }
 
-            if (node.type === 'atrule') {
+            if (node.type === "atrule") {
                 if (node.raws.afterName) {
-                    const commentsReplaced = replaceComments(node.raws.afterName);
+                    const commentsReplaced = replaceComments(
+                        node.raws.afterName
+                    );
+
                     if (!commentsReplaced.length) {
-                        node.raws.afterName = commentsReplaced + ' ';
+                        node.raws.afterName = commentsReplaced + " ";
                     } else {
-                        node.raws.afterName = ' ' + commentsReplaced + ' ';
+                        node.raws.afterName = " " + commentsReplaced + " ";
                     }
                 }
+
                 if (node.raws.params && node.raws.params.raw) {
-                    node.raws.params.raw = replaceComments(node.raws.params.raw);
+                    node.raws.params.raw = replaceComments(
+                        node.raws.params.raw
+                    );
                 }
             }
         });
