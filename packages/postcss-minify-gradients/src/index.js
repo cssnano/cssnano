ecdcddf0569dd9e4d8c589f3a83296d0dffc2e7e
evilebottnawi <sheo13666q@gmail.com>
2019-04-29 15:09:37 +0300

    fix(postcss-minify-gradients): don't break gradients with `var` and `env` functions

--- packages/postcss-minify-gradients/src/index.js
+++ packages/postcss-minify-gradients/src/index.js
@@ -1,173 +1,216 @@
-import postcss from 'postcss';
-import valueParser, {unit, stringify} from 'postcss-value-parser';
-import getArguments from 'lerna:cssnano-util-get-arguments';
-import isColorStop from 'is-color-stop';
+import postcss from "postcss";
+import valueParser, {unit, stringify} from "postcss-value-parser";
+import getArguments from "lerna:cssnano-util-get-arguments";
+import isColorStop from "is-color-stop";
 
 const angles = {
-    top:    '0deg',
-    right:  '90deg',
-    bottom: '180deg',
-    left:   '270deg',
+    top: "0deg",
+    right: "90deg",
+    bottom: "180deg",
+    left: "270deg",
 };
 
 function isLessThan (a, b) {
-    return a.unit.toLowerCase() === b.unit.toLowerCase() && parseFloat(a.number) >= parseFloat(b.number);
+    return (
+        a.unit.toLowerCase() === b.unit.toLowerCase() &&
+        parseFloat(a.number) >= parseFloat(b.number)
+    );
 }
 
 function optimise (decl) {
     const value = decl.value;
 
-    if (!~value.toLowerCase().indexOf('gradient')) {
+    if (!value) {
         return;
     }
 
-    decl.value = valueParser(value).walk(node => {
-        if (node.type !== 'function' || !node.nodes.length) {
-            return false;
-        }
+    const normalizedValue = value.toLowerCase();
 
-        const lowerCasedValue = node.value.toLowerCase();
+    if (normalizedValue.includes("var(") || normalizedValue.includes("env(")) {
+        return;
+    }
 
-        if (
-            lowerCasedValue === 'linear-gradient' ||
-            lowerCasedValue === 'repeating-linear-gradient' ||
-            lowerCasedValue === '-webkit-linear-gradient' ||
-            lowerCasedValue === '-webkit-repeating-linear-gradient'
-        ) {
-            let args = getArguments(node);
+    if (!normalizedValue.includes("gradient")) {
+        return;
+    }
 
-            if (node.nodes[0].value.toLowerCase() === 'to' && args[0].length === 3) {
-                node.nodes = node.nodes.slice(2);
-                node.nodes[0].value = angles[node.nodes[0].value.toLowerCase()];
+    decl.value = valueParser(value)
+        .walk(node => {
+            if (node.type !== "function" || !node.nodes.length) {
+                return false;
             }
 
-            let lastStop = null;
+            const lowerCasedValue = node.value.toLowerCase();
 
-            args.forEach((arg, index) => {
-                if (!arg[2]) {
-                    return;
+            if (
+                lowerCasedValue === "linear-gradient" ||
+                lowerCasedValue === "repeating-linear-gradient" ||
+                lowerCasedValue === "-webkit-linear-gradient" ||
+                lowerCasedValue === "-webkit-repeating-linear-gradient"
+            ) {
+                let args = getArguments(node);
+
+                if (
+                    node.nodes[0].value.toLowerCase() === "to" &&
+                    args[0].length === 3
+                ) {
+                    node.nodes = node.nodes.slice(2);
+                    node.nodes[0].value =
+                        angles[node.nodes[0].value.toLowerCase()];
                 }
 
-                let isFinalStop = index === args.length - 1;
-                let thisStop = unit(arg[2].value);
+                let lastStop = null;
 
-                if (lastStop === null) {
-                    lastStop = thisStop;
-
-                    if (!isFinalStop && lastStop && lastStop.number === '0' && lastStop.unit.toLowerCase() !== 'deg') {
-                        arg[1].value = arg[2].value = '';
+                args.forEach((arg, index) => {
+                    if (!arg[2]) {
+                        return;
                     }
 
-                    return;
-                }
+                    let isFinalStop = index === args.length - 1;
+                    let thisStop = unit(arg[2].value);
 
-                if (lastStop && thisStop && isLessThan(lastStop, thisStop)) {
-                    arg[2].value = 0;
-                }
+                    if (lastStop === null) {
+                        lastStop = thisStop;
 
-                lastStop = thisStop;
+                        if (
+                            !isFinalStop &&
+                            lastStop &&
+                            lastStop.number === "0" &&
+                            lastStop.unit.toLowerCase() !== "deg"
+                        ) {
+                            arg[1].value = arg[2].value = "";
+                        }
 
-                if (isFinalStop && arg[2].value === '100%') {
-                    arg[1].value = arg[2].value = '';
-                }
-            });
+                        return;
+                    }
 
-            return false;
-        }
+                    if (
+                        lastStop &&
+                        thisStop &&
+                        isLessThan(lastStop, thisStop)
+                    ) {
+                        arg[2].value = 0;
+                    }
 
-        if (
-            lowerCasedValue === 'radial-gradient' ||
-            lowerCasedValue === 'repeating-radial-gradient'
-        ) {
-            let args = getArguments(node);
-            let lastStop;
-
-            const hasAt = args[0].find(n => n.value.toLowerCase() === 'at');
-
-            args.forEach((arg, index) => {
-                if (!arg[2] || !index && hasAt) {
-                    return;
-                }
-
-                let thisStop = unit(arg[2].value);
-
-                if (!lastStop) {
                     lastStop = thisStop;
 
-                    return;
-                }
+                    if (isFinalStop && arg[2].value === "100%") {
+                        arg[1].value = arg[2].value = "";
+                    }
+                });
 
-                if (lastStop && thisStop && isLessThan(lastStop, thisStop)) {
-                    arg[2].value = 0;
-                }
+                return false;
+            }
 
-                lastStop = thisStop;
-            });
+            if (
+                lowerCasedValue === "radial-gradient" ||
+                lowerCasedValue === "repeating-radial-gradient"
+            ) {
+                let args = getArguments(node);
+                let lastStop;
 
-            return false;
-        }
+                const hasAt = args[0].find(n => n.value.toLowerCase() === "at");
 
-        if (
-            lowerCasedValue === '-webkit-radial-gradient' ||
-            lowerCasedValue === '-webkit-repeating-radial-gradient'
-        ) {
-            let args = getArguments(node);
-            let lastStop;
+                args.forEach((arg, index) => {
+                    if (!arg[2] || (!index && hasAt)) {
+                        return;
+                    }
 
-            args.forEach((arg) => {
-                let color;
-                let stop;
+                    let thisStop = unit(arg[2].value);
 
-                if (arg[2] !== undefined) {
-                    if (arg[0].type === 'function') {
-                        color = `${arg[0].value}(${stringify(arg[0].nodes)})`;
+                    if (!lastStop) {
+                        lastStop = thisStop;
+
+                        return;
+                    }
+
+                    if (
+                        lastStop &&
+                        thisStop &&
+                        isLessThan(lastStop, thisStop)
+                    ) {
+                        arg[2].value = 0;
+                    }
+
+                    lastStop = thisStop;
+                });
+
+                return false;
+            }
+
+            if (
+                lowerCasedValue === "-webkit-radial-gradient" ||
+                lowerCasedValue === "-webkit-repeating-radial-gradient"
+            ) {
+                let args = getArguments(node);
+                let lastStop;
+
+                args.forEach(arg => {
+                    let color;
+                    let stop;
+
+                    if (arg[2] !== undefined) {
+                        if (arg[0].type === "function") {
+                            color = `${arg[0].value}(${stringify(
+                                arg[0].nodes
+                            )})`;
+                        } else {
+                            color = arg[0].value;
+                        }
+
+                        if (arg[2].type === "function") {
+                            stop = `${arg[2].value}(${stringify(
+                                arg[2].nodes
+                            )})`;
+                        } else {
+                            stop = arg[2].value;
+                        }
                     } else {
+                        if (arg[0].type === "function") {
+                            color = `${arg[0].value}(${stringify(
+                                arg[0].nodes
+                            )})`;
+                        }
+
                         color = arg[0].value;
                     }
 
-                    if (arg[2].type === 'function') {
-                        stop = `${arg[2].value}(${stringify(arg[2].nodes)})`;
-                    } else {
-                        stop = arg[2].value;
+                    color = color.toLowerCase();
+
+                    const colorStop =
+                        stop || stop === 0
+                            ? isColorStop(color, stop.toLowerCase())
+                            : isColorStop(color);
+
+                    if (!colorStop || !arg[2]) {
+                        return;
                     }
-                } else {
-                    if (arg[0].type === 'function') {
-                        color = `${arg[0].value}(${stringify(arg[0].nodes)})`;
+
+                    let thisStop = unit(arg[2].value);
+
+                    if (!lastStop) {
+                        lastStop = thisStop;
+
+                        return;
                     }
 
-                    color = arg[0].value;
-                }
+                    if (
+                        lastStop &&
+                        thisStop &&
+                        isLessThan(lastStop, thisStop)
+                    ) {
+                        arg[2].value = 0;
+                    }
 
-                color = color.toLowerCase();
-
-                const colorStop = stop || stop === 0 ?
-                  isColorStop(color, stop.toLowerCase()) :
-                  isColorStop(color);
-
-                if (!colorStop || !arg[2]) {
-                    return;
-                }
-
-                let thisStop = unit(arg[2].value);
-
-                if (!lastStop) {
                     lastStop = thisStop;
+                });
 
-                    return;
-                }
-
-                if (lastStop && thisStop && isLessThan(lastStop, thisStop)) {
-                    arg[2].value = 0;
-                }
-
-                lastStop = thisStop;
-            });
-
-            return false;
-        }
-    }).toString();
+                return false;
+            }
+        })
+        .toString();
 }
 
-export default postcss.plugin('postcss-minify-gradients', () => {
+export default postcss.plugin("postcss-minify-gradients", () => {
     return css => css.walkDecls(optimise);
 });
