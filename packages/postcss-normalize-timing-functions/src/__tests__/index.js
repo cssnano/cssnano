677bb109744ef2e84e8361848a95ef7ec0a6b33f
evilebottnawi <sheo13666q@gmail.com>
2019-04-30 17:04:36 +0300

    fix(postcss-normalize-timing-functions): do not break invalid syntax

--- packages/postcss-normalize-timing-functions/src/__tests__/index.js
+++ packages/postcss-normalize-timing-functions/src/__tests__/index.js
@@ -5,7 +5,7 @@ import {
   processCSSFactory,
 } from '../../../../util/testHelpers';
 
-const { processCSS } = processCSSFactory(plugin);
+const { processCSS, passthroughCSS } = processCSSFactory(plugin);
 
 function testTimingFunction(t, fixture, expected) {
   return Promise.all([
@@ -39,9 +39,23 @@ function testPassthrough(t, fixture) {
 test(testTimingFunction, 'cubic-bezier(0.25, 0.1, 0.25, 1)', 'ease');
 
 test(
-  'should support uppercase property ',
+  'should support uppercase property',
   processCSS,
   'h1{animation: cubic-bezier(0.25, 0.1, 0.25, 1)}',
+  'h1{animation: ease}'
+);
+
+test(
+  'should support uppercase property #1',
+  processCSS,
+  'h1{animation: cubic-bezier(0.250, 0.10, 0.250, 1)}',
+  'h1{animation: ease}'
+);
+
+test(
+  'should support uppercase property #2',
+  processCSS,
+  'h1{animation: cubic-bezier(0.250, 1e-1px, 0.250, 1)}',
   'h1{animation: ease}'
 );
 
@@ -54,6 +68,8 @@ test(testTimingFunction, 'cubic-bezier(0.42, 0, 1, 1)', 'ease-in');
 test(testTimingFunction, 'cubic-bezier(0, 0, 0.58, 1)', 'ease-out');
 
 test(testTimingFunction, 'cubic-bezier(0.42, 0, 0.58, 1)', 'ease-in-out');
+
+test(testTimingFunction, 'steps(1, start)', 'step-start');
 
 test(testTimingFunction, 'steps(1, start)', 'step-start');
 
@@ -76,5 +92,38 @@ test(testPassthrough, 'steps(15)');
 test(testPassthrough, 'var(--anim1)');
 
 test(testPassthrough, 'VAR(--anim1)');
+
+test(testPassthrough, 'cubic-bezier(0.25, var(--foo), 0.25, 1)');
+
+test(
+  testPassthrough,
+  'cubic-bezier(var(--foo), var(--bar), var(--baz), var(--foz))'
+);
+
+test('should pass through broken syntax', passthroughCSS, 'h1{animation:}');
+
+test(
+  'should pass through broken syntax #1',
+  passthroughCSS,
+  'h1{animation: cubic-bezier()}'
+);
+
+test(
+  'should pass through broken syntax #2',
+  passthroughCSS,
+  'h1{animation: cubic-bezier(0, 0, 1, 1, 1, 1)}'
+);
+
+test(
+  'should pass through broken syntax #3',
+  passthroughCSS,
+  'h1{animation: fade 3s steps(10, end())}'
+);
+
+test(
+  'should pass through broken syntax #4',
+  passthroughCSS,
+  'h1{animation: fade 3s steps(1, start())}'
+);
 
 test('should use the postcss plugin api', usePostCSSPlugin, plugin());
