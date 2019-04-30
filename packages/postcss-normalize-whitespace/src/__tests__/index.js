2fded5c7057c790e3c08a9dca6da5f2167dc4b25
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-04-30 16:13:26 +0300

    test(postcss-normalize-whitespace): var and env functions (#747)

--- packages/postcss-normalize-whitespace/src/__tests__/index.js
+++ packages/postcss-normalize-whitespace/src/__tests__/index.js
@@ -25,6 +25,27 @@ test(
 );
 
 test(
+  'should trim whitespace from css variables',
+  processCSS,
+  'h1{width:var(--foo, calc(10px + 10px))}',
+  'h1{width:var(--foo,calc(10px + 10px))}'
+);
+
+test(
+  'should trim whitespace from env variables',
+  processCSS,
+  'h1{width:env(--foo, calc(10px + 10px))}',
+  'h1{width:env(--foo,calc(10px + 10px))}'
+);
+
+test(
+  'should trim whitespace from var with calc',
+  processCSS,
+  'h1{width:var(--foo, calc(10px * 10px))}',
+  'h1{width:var(--foo,calc(10px * 10px))}'
+);
+
+test(
   'should trim whitespace from nested functions (preset)',
   withDefaultPreset,
   'h1{width:calc(10px - ( 100px / var(--test) ))}',
