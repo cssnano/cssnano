ca14da22921c604815aa25b28faac467ae141310
evilebottnawi <sheo13666q@gmail.com>
2019-04-30 22:12:17 +0300

    fix(postcss-normalize-positions): correct optimize math (`calc` and etc) and variable functions (`var` and `env`)

--- packages/postcss-normalize-positions/src/index.js
+++ packages/postcss-normalize-positions/src/index.js
@@ -1,6 +1,5 @@
 import { plugin } from 'postcss';
 import valueParser, { unit } from 'postcss-value-parser';
-import getArguments from 'lerna:cssnano-util-get-arguments';
 import has from 'has';
 
 const directions = ['top', 'right', 'bottom', 'left', 'center'];
@@ -17,92 +16,168 @@ const vertical = {
   top: '0',
 };
 
+function isCommaNode(node) {
+  return node.type === 'div' && node.value === ',';
+}
+
+function isVariableFunctionNode(node) {
+  if (node.type !== 'function') {
+    return false;
+  }
+
+  return ['var', 'env'].includes(node.value.toLowerCase());
+}
+
+function isMathFunctionNode(node) {
+  if (node.type !== 'function') {
+    return false;
+  }
+
+  return ['calc', 'min', 'max', 'clamp'].includes(node.value.toLowerCase());
+}
+
+function isNumberNode(node) {
+  if (node.type !== 'word') {
+    return false;
+  }
+
+  const value = parseFloat(node.value);
+
+  return !isNaN(value);
+}
+
+function isDimensionNode(node) {
+  if (node.type !== 'word') {
+    return false;
+  }
+
+  const parsed = unit(node.value);
+
+  if (!parsed) {
+    return false;
+  }
+
+  return parsed.unit !== '';
+}
+
 function transform(value) {
   const parsed = valueParser(value);
-  const args = getArguments(parsed);
-  const relevant = [];
+  const ranges = [];
+  let backgroundIndex = 0;
+  let shouldContinue = true;
 
-  args.forEach((arg) => {
-    relevant.push({
-      start: null,
-      end: null,
-    });
+  parsed.nodes.forEach((node, index) => {
+    // After comma (`,`) follows next background
+    if (isCommaNode(node)) {
+      backgroundIndex += 1;
+      shouldContinue = true;
 
-    arg.forEach((part, index) => {
-      const isPosition =
-        ~directions.indexOf(part.value.toLowerCase()) || unit(part.value);
-      const len = relevant.length - 1;
+      return;
+    }
 
-      if (relevant[len].start === null && isPosition) {
-        relevant[len].start = index;
-        relevant[len].end = index;
+    if (!shouldContinue) {
+      return;
+    }
+
+    // After separator (`/`) follows `background-size` values
+    // Avoid their
+    if (node.type === 'div' && node.value === '/') {
+      shouldContinue = false;
+
+      return;
+    }
+
+    if (!ranges[backgroundIndex]) {
+      ranges[backgroundIndex] = {
+        start: null,
+        end: null,
+      };
+    }
+
+    // Do not try to be processed `var and `env` function inside background
+    if (isVariableFunctionNode(node)) {
+      shouldContinue = false;
+      ranges[backgroundIndex].start = null;
+      ranges[backgroundIndex].end = null;
+
+      return;
+    }
+
+    const isPosition =
+      (node.type === 'word' && directions.includes(node.value.toLowerCase())) ||
+      isDimensionNode(node) ||
+      isNumberNode(node) ||
+      isMathFunctionNode(node);
+
+    if (ranges[backgroundIndex].start === null && isPosition) {
+      ranges[backgroundIndex].start = index;
+      ranges[backgroundIndex].end = index;
+
+      return;
+    }
+
+    if (ranges[backgroundIndex].start !== null) {
+      if (node.type === 'space') {
+        return;
+      } else if (isPosition) {
+        ranges[backgroundIndex].end = index;
 
         return;
       }
 
-      if (relevant[len].start !== null) {
-        if (part.type === 'space') {
-          return;
-        } else if (isPosition) {
-          relevant[len].end = index;
-
-          return;
-        }
-
-        return;
-      }
-    });
+      return;
+    }
   });
 
-  relevant.forEach((range, index) => {
+  ranges.forEach((range) => {
     if (range.start === null) {
       return;
     }
 
-    const position = args[index].slice(range.start, range.end + 1);
+    const nodes = parsed.nodes.slice(range.start, range.end + 1);
 
-    if (position.length > 3) {
+    if (nodes.length > 3) {
       return;
     }
 
-    const firstValue = position[0].value.toLowerCase();
-    const secondValue =
-      position[2] && position[2].value ? position[2].value.toLowerCase() : null;
+    const firstNode = nodes[0].value.toLowerCase();
+    const secondNode =
+      nodes[2] && nodes[2].value ? nodes[2].value.toLowerCase() : null;
 
-    if (position.length === 1 || secondValue === 'center') {
-      if (secondValue) {
-        position[2].value = position[1].value = '';
+    if (nodes.length === 1 || secondNode === 'center') {
+      if (secondNode) {
+        nodes[2].value = nodes[1].value = '';
       }
 
       const map = Object.assign({}, horizontal, {
         center,
       });
 
-      if (has(map, firstValue)) {
-        position[0].value = map[firstValue];
+      if (has(map, firstNode)) {
+        nodes[0].value = map[firstNode];
       }
 
       return;
     }
 
-    if (firstValue === 'center' && ~directions.indexOf(secondValue)) {
-      position[0].value = position[1].value = '';
+    if (firstNode === 'center' && directions.includes(secondNode)) {
+      nodes[0].value = nodes[1].value = '';
 
-      if (has(horizontal, secondValue)) {
-        position[2].value = horizontal[secondValue];
+      if (has(horizontal, secondNode)) {
+        nodes[2].value = horizontal[secondNode];
       }
 
       return;
     }
 
-    if (has(horizontal, firstValue) && has(vertical, secondValue)) {
-      position[0].value = horizontal[firstValue];
-      position[2].value = vertical[secondValue];
+    if (has(horizontal, firstNode) && has(vertical, secondNode)) {
+      nodes[0].value = horizontal[firstNode];
+      nodes[2].value = vertical[secondNode];
 
       return;
-    } else if (has(vertical, firstValue) && has(horizontal, secondValue)) {
-      position[0].value = horizontal[secondValue];
-      position[2].value = vertical[firstValue];
+    } else if (has(vertical, firstNode) && has(horizontal, secondNode)) {
+      nodes[0].value = horizontal[secondNode];
+      nodes[2].value = vertical[firstNode];
 
       return;
     }
@@ -116,9 +191,13 @@ export default plugin('postcss-normalize-positions', () => {
     const cache = {};
 
     css.walkDecls(
-      /^(background(-position)?|(-webkit-)?perspective-origin)$/i,
+      /^(background(-position)?|(-\w+-)?perspective-origin)$/i,
       (decl) => {
         const value = decl.value;
+
+        if (!value) {
+          return;
+        }
 
         if (cache[value]) {
           decl.value = cache[value];
