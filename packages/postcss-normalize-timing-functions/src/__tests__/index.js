44f794a9075525faccfc1a6a8a1fe1148a440017
evilebottnawi <sheo13666q@gmail.com>
2019-05-06 19:54:14 +0300

    refactor(postcss-normalize-timing-functions): code

--- packages/postcss-normalize-timing-functions/src/__tests__/index.js
+++ packages/postcss-normalize-timing-functions/src/__tests__/index.js
@@ -16,8 +16,18 @@ function testTimingFunction(t, fixture, expected) {
     ),
     processCSS(
       t,
+      `-webkit-animation:fade 3s ${fixture}`,
+      `-webkit-animation:fade 3s ${expected}`
+    ),
+    processCSS(
+      t,
       `animation-timing-function:${fixture}`,
       `animation-timing-function:${expected}`
+    ),
+    processCSS(
+      t,
+      `-webkit-animation-timing-function:${fixture}`,
+      `-webkit-animation-timing-function:${expected}`
     ),
     processCSS(
       t,
@@ -26,8 +36,18 @@ function testTimingFunction(t, fixture, expected) {
     ),
     processCSS(
       t,
+      `-webkit-transition:color 3s ${fixture}`,
+      `-webkit-transition:color 3s ${expected}`
+    ),
+    processCSS(
+      t,
       `transition-timing-function:${fixture}`,
       `transition-timing-function:${expected}`
+    ),
+    processCSS(
+      t,
+      `-webkit-transition-timing-function:${fixture}`,
+      `-webkit-transition-timing-function:${expected}`
     ),
   ]);
 }
@@ -37,27 +57,6 @@ function testPassthrough(t, fixture) {
 }
 
 test(testTimingFunction, 'cubic-bezier(0.25, 0.1, 0.25, 1)', 'ease');
-
-test(
-  'should support uppercase property',
-  processCSS,
-  'h1{animation: cubic-bezier(0.25, 0.1, 0.25, 1)}',
-  'h1{animation: ease}'
-);
-
-test(
-  'should support uppercase property #1',
-  processCSS,
-  'h1{animation: cubic-bezier(0.250, 0.10, 0.250, 1)}',
-  'h1{animation: ease}'
-);
-
-test(
-  'should support uppercase property #2',
-  processCSS,
-  'h1{animation: cubic-bezier(0.250, 1e-1px, 0.250, 1)}',
-  'h1{animation: ease}'
-);
 
 test(testTimingFunction, 'CUBIC-BEZIER(0.25, 0.1, 0.25, 1)', 'ease');
 
@@ -71,11 +70,27 @@ test(testTimingFunction, 'cubic-bezier(0.42, 0, 0.58, 1)', 'ease-in-out');
 
 test(testTimingFunction, 'steps(1, start)', 'step-start');
 
-test(testTimingFunction, 'steps(1, start)', 'step-start');
-
 test(testTimingFunction, 'steps(1, START)', 'step-start');
 
 test(testTimingFunction, 'STEPS(1, start)', 'step-start');
+
+test(testTimingFunction, 'steps(1, jump-start)', 'step-start');
+
+test(testTimingFunction, 'steps(1, JUMP-START)', 'step-start');
+
+test(testTimingFunction, 'STEPS(1, jump-start)', 'step-start');
+
+test(testTimingFunction, 'steps(1, end)', 'step-end');
+
+test(testTimingFunction, 'steps(1, END)', 'step-end');
+
+test(testTimingFunction, 'STEPS(1, end)', 'step-end');
+
+test(testTimingFunction, 'steps(1, jump-end)', 'step-end');
+
+test(testTimingFunction, 'steps(1, JUMP-END)', 'step-end');
+
+test(testTimingFunction, 'STEPS(1, jump-end)', 'step-end');
 
 test(testPassthrough, 'steps(1)');
 
@@ -85,7 +100,11 @@ test(testPassthrough, 'steps(5,start)');
 
 test(testTimingFunction, 'steps(10, end)', 'steps(10)');
 
+test(testTimingFunction, 'steps(10, jump-end)', 'steps(10)');
+
 test(testTimingFunction, 'steps(10, END)', 'steps(10)');
+
+test(testTimingFunction, 'steps(10, JUMP-END)', 'steps(10)');
 
 test(testPassthrough, 'steps(15)');
 
@@ -102,28 +121,36 @@ test(
 
 test('should pass through broken syntax', passthroughCSS, 'h1{animation:}');
 
+test(testPassthrough, 'cubic-bezier()');
+
+test(testPassthrough, 'cubic-bezier(0, 0, 1, 1, 1, 1)');
+
+test(testPassthrough, 'fade 3s steps(10, start())');
+
+test(testPassthrough, 'fade 3s steps(10, jump-start())');
+
+test(testPassthrough, 'fade 3s steps(10, end())');
+
+test(testPassthrough, 'fade 3s steps(10, jump-end())');
+
+test(testTimingFunction, 'cubic-bezier(0.25, 0.1, 0.25, 1)', 'ease');
+
+test(testTimingFunction, 'cubic-bezier(0.250, 0.10, 0.250, 1)', 'ease');
+
+test(testTimingFunction, 'cubic-bezier(0.250, 1e-1px, 0.250, 1)', 'ease');
+
 test(
-  'should pass through broken syntax #1',
-  passthroughCSS,
-  'h1{animation: cubic-bezier()}'
+  'should supports multiple timing functions (animation-timing-function)',
+  processCSS,
+  'h1{animation-timing-function: cubic-bezier(0, 0, 1, 1), cubic-bezier(0, 0, 1, 1), steps(1, start)}',
+  'h1{animation-timing-function: linear, linear, step-start}'
 );
 
 test(
-  'should pass through broken syntax #2',
-  passthroughCSS,
-  'h1{animation: cubic-bezier(0, 0, 1, 1, 1, 1)}'
-);
-
-test(
-  'should pass through broken syntax #3',
-  passthroughCSS,
-  'h1{animation: fade 3s steps(10, end())}'
-);
-
-test(
-  'should pass through broken syntax #4',
-  passthroughCSS,
-  'h1{animation: fade 3s steps(1, start())}'
+  'should supports multiple timing functions (transition-timing-function)',
+  processCSS,
+  'h1{transition-timing-function: cubic-bezier(0, 0, 1, 1), cubic-bezier(0, 0, 1, 1), steps(1, start)}',
+  'h1{transition-timing-function: linear, linear, step-start}'
 );
 
 test('should use the postcss plugin api', usePostCSSPlugin, plugin());
