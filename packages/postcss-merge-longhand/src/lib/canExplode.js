365243025f3efaefd11f264f7223f6048899fd50
Ben Briggs <beneb.info@gmail.com>
2019-05-23 11:01:13 +0100

    fix(postcss-merge-longhand): resolve crash on empty declarations (#770)

--- packages/postcss-merge-longhand/src/lib/canExplode.js
+++ packages/postcss-merge-longhand/src/lib/canExplode.js
@@ -1,13 +1,17 @@
 import isCustomProp from './isCustomProp';
 
-const hasInherit = (node) => node.value.toLowerCase().includes('inherit');
-const hasInitial = (node) => node.value.toLowerCase().includes('initial');
-const hasUnset = (node) => node.value.toLowerCase().includes('unset');
+const hasGlobalKeyword = (prop) =>
+  prop &&
+  prop.value &&
+  ['inherit', 'initial', 'unset', 'revert'].includes(prop.value.toLowerCase());
 
 export default (prop, includeCustomProps = true) => {
-  if (includeCustomProps && isCustomProp(prop)) {
+  if (
+    !prop.value ||
+    (includeCustomProps && isCustomProp(prop)) ||
+    hasGlobalKeyword(prop)
+  ) {
     return false;
   }
-
-  return !hasInherit(prop) && !hasInitial(prop) && !hasUnset(prop);
+  return true;
 };
