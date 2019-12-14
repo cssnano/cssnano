cdffcc93fcf3d01d70a44d1acc0be537d9e4d523
anikethsaha <anik220798@gmail.com>
2019-12-14 18:08:02 +0530

    fix(postcss-unique-selectors): removed comments

--- packages/postcss-unique-selectors/src/index.js
+++ packages/postcss-unique-selectors/src/index.js
@@ -1,11 +1,22 @@
 import { plugin } from 'postcss';
 import sort from 'alphanum-sort';
 import uniqs from 'uniqs';
+import selectorParser from 'postcss-selector-parser';
+
+function parseSelectors(selectors, callback) {
+  return selectorParser(callback).processSync(selectors);
+}
 
 function unique(rule) {
   rule.selector = sort(uniqs(rule.selectors), { insensitive: true }).join();
 }
 
 export default plugin('postcss-unique-selectors', () => {
-  return (css) => css.walkRules(unique);
+  return (css) =>
+    css.walkRules((nodes) => {
+      nodes.selector = parseSelectors(nodes.selector, (selNode) => {
+        selNode.walk((sel) => (sel.type === 'comment' ? sel.remove() : sel));
+      });
+      unique(nodes);
+    });
 });
