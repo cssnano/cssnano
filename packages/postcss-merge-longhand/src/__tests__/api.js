4b4b85e435982415caf712caa48843eb4848acdc
Anix <anik220798@gmail.com>
2020-09-23 13:21:02 +0000

    update: refactor merge-longhand to postcss 8

--- packages/postcss-merge-longhand/src/__tests__/api.js
+++ packages/postcss-merge-longhand/src/__tests__/api.js
@@ -2,6 +2,5 @@ import { name } from '../../package.json';
 import plugin from '..';
 
 test('should use the postcss plugin api', () => {
-  expect(plugin().postcssVersion).toBeDefined();
   expect(plugin().postcssPlugin).toBe(name);
 });
--- packages/postcss-merge-longhand/src/index.js
+++ packages/postcss-merge-longhand/src/index.js
@@ -1,13 +1,19 @@
-import postcss from 'postcss';
 import processors from './lib/decl';
 
-export default postcss.plugin('postcss-merge-longhand', () => {
-  return (css) => {
-    css.walkRules((rule) => {
-      processors.forEach((p) => {
-        p.explode(rule);
-        p.merge(rule);
+const postcssPlugin = 'postcss-merge-longhand';
+
+export default () => {
+  return {
+    postcssPlugin,
+    Root(css) {
+      css.walkRules((rule) => {
+        processors.forEach((p) => {
+          p.explode(rule);
+          p.merge(rule);
+        });
       });
-    });
+    },
   };
-});
+};
+
+export const postcss = true;
