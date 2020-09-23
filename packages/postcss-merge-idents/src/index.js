5d65bfea6231a1f8b5d8326bbda6462c03eee0d7
Anix <anik220798@gmail.com>
2020-09-23 13:04:27 +0000

    update: refactor merge-idents to postcss 8

--- packages/postcss-merge-idents/src/index.js
+++ packages/postcss-merge-idents/src/index.js
@@ -1,7 +1,8 @@
 import has from 'has';
-import { plugin } from 'postcss';
 import valueParser from 'postcss-value-parser';
 import { sameParent } from 'lerna:cssnano-utils';
+
+const postcssPlugin = 'postcss-merge-idents';
 
 function canonical(obj) {
   // Prevent potential infinite loops
@@ -20,7 +21,17 @@ function canonical(obj) {
   };
 }
 
-function mergeAtRules(css, pairs) {
+export default () => {
+  let pairs = [
+    {
+      atrule: /keyframes/i,
+      decl: /animation/i,
+    },
+    {
+      atrule: /counter-style/i,
+      decl: /(list-style|system)/i,
+    },
+  ];
   pairs.forEach((pair) => {
     pair.cache = [];
     pair.replacements = [];
@@ -29,80 +40,60 @@ function mergeAtRules(css, pairs) {
   });
 
   let relevant;
-
-  css.walk((node) => {
-    if (node.type === 'atrule') {
+  return {
+    postcssPlugin,
+    AtRule(node) {
       relevant = pairs.filter((pair) =>
         pair.atrule.test(node.name.toLowerCase())
       )[0];
 
-      if (!relevant) {
-        return;
+      if (relevant) {
+        if (relevant.cache.length < 1) {
+          relevant.cache.push(node);
+        } else {
+          let toString = node.nodes.toString();
+
+          relevant.cache.forEach((cached) => {
+            if (
+              cached.name.toLowerCase() === node.name.toLowerCase() &&
+              sameParent(cached, node) &&
+              cached.nodes.toString() === toString
+            ) {
+              relevant.removals.push(cached);
+              relevant.replacements[cached.params] = node.params;
+            }
+          });
+
+          relevant.cache.push(node);
+        }
       }
-
-      if (relevant.cache.length < 1) {
-        relevant.cache.push(node);
-        return;
-      } else {
-        let toString = node.nodes.toString();
-
-        relevant.cache.forEach((cached) => {
-          if (
-            cached.name.toLowerCase() === node.name.toLowerCase() &&
-            sameParent(cached, node) &&
-            cached.nodes.toString() === toString
-          ) {
-            relevant.removals.push(cached);
-            relevant.replacements[cached.params] = node.params;
-          }
-        });
-
-        relevant.cache.push(node);
-
-        return;
-      }
-    }
-
-    if (node.type === 'decl') {
+    },
+    Declaration(node) {
       relevant = pairs.filter((pair) =>
         pair.decl.test(node.prop.toLowerCase())
       )[0];
 
-      if (!relevant) {
-        return;
+      if (relevant) {
+        relevant.decls.push(node);
       }
+    },
+    RootExit() {
+      pairs.forEach((pair) => {
+        let canon = canonical(pair.replacements);
 
-      relevant.decls.push(node);
-    }
-  });
-
-  pairs.forEach((pair) => {
-    let canon = canonical(pair.replacements);
-
-    pair.decls.forEach((decl) => {
-      decl.value = valueParser(decl.value)
-        .walk((node) => {
-          if (node.type === 'word') {
-            node.value = canon(node.value);
-          }
-        })
-        .toString();
-    });
-    pair.removals.forEach((cached) => cached.remove());
-  });
-}
-
-export default plugin('postcss-merge-idents', () => {
-  return (css) => {
-    mergeAtRules(css, [
-      {
-        atrule: /keyframes/i,
-        decl: /animation/i,
-      },
-      {
-        atrule: /counter-style/i,
-        decl: /(list-style|system)/i,
-      },
-    ]);
+        pair.decls.forEach((decl) => {
+          decl.value = valueParser(decl.value)
+            .walk((node) => {
+              if (node.type === 'word') {
+                node.value = canon(node.value);
+              }
+            })
+            .toString();
+        });
+        pair.removals.forEach((cached) => cached.remove());
+      });
+    },
   };
-});
+};
+
+export const postcss = true;
