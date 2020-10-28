b28b60a88b61b331606de324f50b3c49c27f2903
Ludovico Fischer <livrerie@gmail.com>
2020-10-28 16:46:35 +0100

    update: refactor normalize-whitespace to postcss 8

--- packages/postcss-normalize-whitespace/src/index.js
+++ packages/postcss-normalize-whitespace/src/index.js
@@ -1,9 +1,4 @@
-import { plugin } from 'postcss';
 import valueParser from 'postcss-value-parser';
-
-const atrule = 'atrule';
-const decl = 'decl';
-const rule = 'rule';
 
 function reduceCalcWhitespaces(node) {
   if (node.type === 'space') {
@@ -31,57 +26,69 @@ function reduceWhitespaces(node) {
   }
 }
 
-export default plugin('postcss-normalize-whitespace', () => {
-  return (css) => {
-    const cache = {};
-
-    css.walk((node) => {
-      const { type } = node;
-
-      if (~[decl, rule, atrule].indexOf(type) && node.raws.before) {
-        node.raws.before = node.raws.before.replace(/\s/g, '');
-      }
-
-      if (type === decl) {
-        // Ensure that !important values do not have any excess whitespace
-        if (node.important) {
-          node.raws.important = '!important';
-        }
-
-        // Remove whitespaces around ie 9 hack
-        node.value = node.value.replace(/\s*(\\9)\s*/, '$1');
-
-        const value = node.value;
-
-        if (cache[value]) {
-          node.value = cache[value];
-        } else {
-          const parsed = valueParser(node.value);
-          const result = parsed.walk(reduceWhitespaces).toString();
-
-          // Trim whitespace inside functions & dividers
-          node.value = result;
-          cache[value] = result;
-        }
-
-        // Remove extra semicolons and whitespace before the declaration
-        if (node.raws.before) {
-          const prev = node.prev();
-
-          if (prev && prev.type !== rule) {
-            node.raws.before = node.raws.before.replace(/;/g, '');
+const pluginCreator = () => {
+  return {
+    postcssPlugin: 'postcss-normalize-whitespace',
+    prepare() {
+      const cache = {};
+      return {
+        Declaration(node) {
+          if (node.raws.before) {
+            node.raws.before = node.raws.before.replace(/\s/g, '');
           }
-        }
+          // Ensure that !important values do not have any excess whitespace
+          if (node.important) {
+            node.raws.important = '!important';
+          }
 
-        node.raws.between = ':';
-        node.raws.semicolon = false;
-      } else if (type === rule || type === atrule) {
-        node.raws.between = node.raws.after = '';
-        node.raws.semicolon = false;
-      }
-    });
+          // Remove whitespaces around ie 9 hack
+          node.value = node.value.replace(/\s*(\\9)\s*/, '$1');
 
-    // Remove final newline
-    css.raws.after = '';
+          const value = node.value;
+
+          if (cache[value]) {
+            node.value = cache[value];
+          } else {
+            const parsed = valueParser(node.value);
+            const result = parsed.walk(reduceWhitespaces).toString();
+
+            // Trim whitespace inside functions & dividers
+            node.value = result;
+            cache[value] = result;
+          }
+
+          // Remove extra semicolons and whitespace before the declaration
+          if (node.raws.before) {
+            const prev = node.prev();
+
+            if (prev && prev.type !== 'rule') {
+              node.raws.before = node.raws.before.replace(/;/g, '');
+            }
+          }
+
+          node.raws.between = ':';
+          node.raws.semicolon = false;
+        },
+        Rule(node) {
+          if (node.raws.before) {
+            node.raws.before = node.raws.before.replace(/\s/g, '');
+          }
+
+          node.raws.between = node.raws.after = '';
+          node.raws.semicolon = false;
+        },
+        AtRule(node) {
+          node.raws.between = node.raws.after = '';
+          node.raws.semicolon = false;
+        },
+        OnceExit(css) {
+          css.raws.after = '';
+        },
+      };
+    },
   };
-});
+};
+
+pluginCreator.postcss = true;
+
+export default pluginCreator;
