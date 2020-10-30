96f4d606d4ca00d8088edff7b2fb7293d46e3a17
Ludovico Fischer <livrerie@gmail.com>
2020-10-30 20:13:32 +0100

    update: refactor stylehacks to postcss 8

--- packages/stylehacks/src/index.js
+++ packages/stylehacks/src/index.js
@@ -1,43 +1,45 @@
-import postcss from 'postcss';
 import browserslist from 'browserslist';
 import plugins from './plugins';
 
-const stylehacks = postcss.plugin('stylehacks', (opts = {}) => {
-  return (css, result) => {
-    const resultOpts = result.opts || {};
-    const browsers = browserslist(null, {
-      stats: resultOpts.stats,
-      path: __dirname,
-      env: resultOpts.env,
-    });
-    const processors = plugins.reduce((list, Plugin) => {
-      const hack = new Plugin(result);
-      const applied = browsers.some((browser) => {
-        return hack.targets.some((target) => browser === target);
+const stylehacks = (opts = {}) => {
+  return {
+    postcssPlugin: 'stylehacks',
+    Once(css, { result }) {
+      const resultOpts = result.opts || {};
+      const browsers = browserslist(null, {
+        stats: resultOpts.stats,
+        path: __dirname,
+        env: resultOpts.env,
       });
+      const processors = plugins.reduce((list, Plugin) => {
+        const hack = new Plugin(result);
+        const applied = browsers.some((browser) => {
+          return hack.targets.some((target) => browser === target);
+        });
 
-      if (applied) {
-        return list;
-      }
-
-      return [...list, hack];
-    }, []);
-
-    css.walk((node) => {
-      processors.forEach((proc) => {
-        if (!~proc.nodeTypes.indexOf(node.type)) {
-          return;
+        if (applied) {
+          return list;
         }
 
-        if (opts.lint) {
-          return proc.detectAndWarn(node);
-        }
+        return [...list, hack];
+      }, []);
 
-        return proc.detectAndResolve(node);
+      css.walk((node) => {
+        processors.forEach((proc) => {
+          if (!~proc.nodeTypes.indexOf(node.type)) {
+            return;
+          }
+
+          if (opts.lint) {
+            return proc.detectAndWarn(node);
+          }
+
+          return proc.detectAndResolve(node);
+        });
       });
-    });
+    },
   };
-});
+};
 
 stylehacks.detect = (node) => {
   return plugins.some((Plugin) => {
@@ -47,4 +49,5 @@ stylehacks.detect = (node) => {
   });
 };
 
+stylehacks.postcss = true;
 export default stylehacks;
