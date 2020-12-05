dc37d1a65a4b6de5c8493fd77cd9ffa36a0b429b
Ludovico Fischer <livrerie@gmail.com>
2020-12-05 21:23:02 +0100

    chore(tests): update postcss-font-magician test snapshots.

--- packages/cssnano/src/__tests__/issue315.js
+++ packages/cssnano/src/__tests__/issue315.js
@@ -8,11 +8,11 @@ test('should work with postcss-font-magician', () => {
       font-family: "Alice";
     }
     `;
-  return postcss([fontMagician, cssnano])
+  return postcss([fontMagician({}), cssnano()])
     .process(css, { from: undefined })
     .then((result) => {
       expect(result.css).toMatchInlineSnapshot(
-        `"@font-face{src:local(\\"Alice Regular\\"),local(Alice-Regular),url(//fonts.gstatic.com/s/alice/v11/OpNCnoEEmtHa6GcOrgo.eot#) format(\\"eot\\"),url(//fonts.gstatic.com/s/alice/v11/OpNCnoEEmtHa6GcOrg4.woff2) format(\\"woff2\\"),url(//fonts.gstatic.com/s/alice/v11/OpNCnoEEmtHa6GcOrgg.woff) format(\\"woff\\");font-family:Alice;font-style:normal;font-weight:400}body{font-family:Alice}"`
+        `"@font-face{font-family:Alice;font-style:normal;font-weight:400;src:local(\\"Alice Regular\\"),local(Alice-Regular),url(//fonts.gstatic.com/s/alice/v12/OpNCnoEEmtHa6GcOrgo.eot#) format(\\"eot\\"),url(//fonts.gstatic.com/s/alice/v12/OpNCnoEEmtHa6GcOrg4.woff2) format(\\"woff2\\"),url(//fonts.gstatic.com/s/alice/v12/OpNCnoEEmtHa6GcOrgg.woff) format(\\"woff\\")}body{font-family:Alice}"`
       );
     });
 });
