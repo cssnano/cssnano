0b18c18055932604e72ea6a78af122a36f9dfa60
Ludovico Fischer <livrerie@gmail.com>
2020-12-22 20:15:57 +0100

    update: refactor unique-selectors to postcss 8

--- packages/postcss-unique-selectors/src/index.js
+++ packages/postcss-unique-selectors/src/index.js
@@ -1,4 +1,3 @@
-import { plugin } from 'postcss';
 import sort from 'alphanum-sort';
 import uniqs from 'uniqs';
 import selectorParser from 'postcss-selector-parser';
@@ -11,22 +10,29 @@ function unique(rule) {
   rule.selector = sort(uniqs(rule.selectors), { insensitive: true }).join();
 }
 
-export default plugin('postcss-unique-selectors', () => {
-  return (css) =>
-    css.walkRules((nodes) => {
-      let comments = [];
-      nodes.selector = parseSelectors(nodes.selector, (selNode) => {
-        selNode.walk((sel) => {
-          if (sel.type === 'comment') {
-            comments.push(sel.value);
-            sel.remove();
-            return;
-          } else {
-            return sel;
-          }
+function pluginCreator() {
+  return {
+    postcssPlugin: 'postcss-unique-selectors',
+    OnceExit(css) {
+      css.walkRules((nodes) => {
+        let comments = [];
+        nodes.selector = parseSelectors(nodes.selector, (selNode) => {
+          selNode.walk((sel) => {
+            if (sel.type === 'comment') {
+              comments.push(sel.value);
+              sel.remove();
+              return;
+            } else {
+              return sel;
+            }
+          });
         });
+        unique(nodes);
+        nodes.selectors = nodes.selectors.concat(comments);
       });
-      unique(nodes);
-      nodes.selectors = nodes.selectors.concat(comments);
-    });
-});
+    },
+  };
+}
+
+pluginCreator.postcss = true;
+export default pluginCreator;
