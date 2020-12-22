6118f91d1f365e764e869e40f0b49028243b9e2c
Ludovico Fischer <livrerie@gmail.com>
2020-12-22 15:33:45 +0100

    update: refactor reduce-initial to postcss 8

--- packages/postcss-reduce-initial/src/index.js
+++ packages/postcss-reduce-initial/src/index.js
@@ -1,5 +1,3 @@
-import { plugin } from 'postcss';
-import has from 'has';
 import browserslist from 'browserslist';
 import { isSupported } from 'caniuse-api';
 import fromInitial from '../data/fromInitial.json';
@@ -10,42 +8,54 @@ const initial = 'initial';
 // In most of the browser including chrome the initial for `writing-mode` is not `horizontal-tb`. Ref https://github.com/cssnano/cssnano/pull/905
 const defaultIgnoreProps = ['writing-mode'];
 
-export default plugin('postcss-reduce-initial', () => {
-  return (css, result) => {
-    const resultOpts = result.opts || {};
-    const browsers = browserslist(null, {
-      stats: resultOpts.stats,
-      path: __dirname,
-      env: resultOpts.env,
-    });
+function pluginCreator() {
+  return {
+    postcssPlugin: 'postcss-reduce-initial',
 
-    const initialSupport = isSupported('css-initial-value', browsers);
+    prepare(result) {
+      const resultOpts = result.opts || {};
+      const browsers = browserslist(null, {
+        stats: resultOpts.stats,
+        path: __dirname,
+        env: resultOpts.env,
+      });
 
-    css.walkDecls((decl) => {
-      const lowerCasedProp = decl.prop.toLowerCase();
-      const ignoreProp = defaultIgnoreProps.concat(resultOpts.ignore || []);
+      const initialSupport = isSupported('css-initial-value', browsers);
+      return {
+        OnceExit(css) {
+          css.walkDecls((decl) => {
+            const lowerCasedProp = decl.prop.toLowerCase();
+            const ignoreProp = defaultIgnoreProps.concat(
+              resultOpts.ignore || []
+            );
 
-      if (ignoreProp.includes(lowerCasedProp)) {
-        return;
-      }
+            if (ignoreProp.includes(lowerCasedProp)) {
+              return;
+            }
 
-      if (
-        initialSupport &&
-        has(toInitial, lowerCasedProp) &&
-        decl.value.toLowerCase() === toInitial[lowerCasedProp]
-      ) {
-        decl.value = initial;
-        return;
-      }
+            if (
+              initialSupport &&
+              Object.prototype.hasOwnProperty.call(toInitial, lowerCasedProp) &&
+              decl.value.toLowerCase() === toInitial[lowerCasedProp]
+            ) {
+              decl.value = initial;
+              return;
+            }
 
-      if (
-        decl.value.toLowerCase() !== initial ||
-        !fromInitial[lowerCasedProp]
-      ) {
-        return;
-      }
+            if (
+              decl.value.toLowerCase() !== initial ||
+              !fromInitial[lowerCasedProp]
+            ) {
+              return;
+            }
 
-      decl.value = fromInitial[lowerCasedProp];
-    });
+            decl.value = fromInitial[lowerCasedProp];
+          });
+        },
+      };
+    },
   };
-});
+}
+
+pluginCreator.postcss = true;
+export default pluginCreator;
