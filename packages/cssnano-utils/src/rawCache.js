06c4aabaa8c3f9ef0135fffa2b4bacccc41dd35a
Alexey Pyltsyn <lex61rus@gmail.com>
2021-03-04 12:50:29 +0300

    Migrate cssnano-util-raw-cache to PostCSS 8

--- packages/cssnano-utils/src/rawCache.js
+++ packages/cssnano-utils/src/rawCache.js
@@ -1,19 +1,24 @@
-import { plugin } from 'postcss';
-
-export default plugin('cssnano-util-raw-cache', () => {
-  return (css, result) => {
-    result.root.rawCache = {
-      colon: ':',
-      indent: '',
-      beforeDecl: '',
-      beforeRule: '',
-      beforeOpen: '',
-      beforeClose: '',
-      beforeComment: '',
-      after: '',
-      emptyBody: '',
-      commentLeft: '',
-      commentRight: '',
-    };
+const pluginCreator = () => {
+  return {
+    postcssPlugin: 'cssnano-util-raw-cache',
+    OnceExit(css, { result }) {
+      result.root.rawCache = {
+        colon: ':',
+        indent: '',
+        beforeDecl: '',
+        beforeRule: '',
+        beforeOpen: '',
+        beforeClose: '',
+        beforeComment: '',
+        after: '',
+        emptyBody: '',
+        commentLeft: '',
+        commentRight: '',
+      };
+    },
   };
-});
+};
+
+pluginCreator.postcss = true;
+
+export default pluginCreator;
