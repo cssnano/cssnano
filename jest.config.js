28e2b4b836529f4d35beebf61c058ae2463c5317
Sigve Hoel <6737410+sigveio@users.noreply.github.com>
2021-07-07 21:20:47 +0200

    test(cssnano): ignore test helpers in coverage rep

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     'packages/*/src/**/*.js',
     '!packages/postcss-colormin/src/generate.js',
     '!packages/postcss-reduce-initial/src/acquire.js',
+    '!packages/cssnano/src/__tests__/_processCss.js',
+    '!packages/cssnano/src/__tests__/_webpack.config.js',
   ],
   testPathIgnorePatterns: [
     '/node_modules/',
