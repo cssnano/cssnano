fb67d42e64f6f2073fbc4346656ddffbe35c29b9
Kuba <kuba.marek@infermedica.com>
2021-12-14 03:59:52 +0100

    feature: allow options in colormin

--- packages/postcss-colormin/src/minifyColor.js
+++ packages/postcss-colormin/src/minifyColor.js
@@ -8,25 +8,14 @@ extend([namesPlugin, minifierPlugin]);
  * Performs color value minification
  *
  * @param {string} input - CSS value
- * @param {boolean} options.supportsAlphaHex - Does the browser support 4 & 8 character hex notation
- * @param {boolean} options.supportsTransparent – Does the browser support "transparent" value properly
+ * @param {boolean} options - object with colord.minify() options
  */
 export default function minifyColor(input, options = {}) {
-  const settings = {
-    supportsAlphaHex: false,
-    supportsTransparent: true,
-    ...options,
-  };
-
   const instance = colord(input);
 
   if (instance.isValid()) {
     // Try to shorten the string if it is a valid CSS color value
-    const minified = instance.minify({
-      alphaHex: settings.supportsAlphaHex,
-      transparent: settings.supportsTransparent,
-      name: true,
-    });
+    const minified = instance.minify(options);
 
     // Fall back to the original input if it's smaller or has equal length
     return minified.length < input.length ? minified : input.toLowerCase();
