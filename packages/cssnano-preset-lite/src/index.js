85d1ecdafe021ecf0fd8dd736878b13912565d22
Ludovico Fischer <livrerie@gmail.com>
2022-02-16 02:32:37 +0100

    feat(cssnano-preset-lite): add types

--- packages/cssnano-preset-lite/src/index.js
+++ packages/cssnano-preset-lite/src/index.js
@@ -4,11 +4,20 @@ const postcssNormalizeWhitespace = require('postcss-normalize-whitespace');
 const postcssDiscardEmpty = require('postcss-discard-empty');
 const { rawCache } = require('cssnano-utils');
 
-const defaultOpts = {};
+/** @typedef {false | { exclude?: true} }SimpleOptions */
+/** @typedef {{discardComments?: false | import('postcss-discard-comments').Options & { exclude?: true},
+normalizeWhitespace?: SimpleOptions, 
+discardEmpty?: SimpleOptions, 
+rawCache?: SimpleOptions}} Options */
 
+const defaultOpts = {};
+/**
+ * @param {Options} opts
+ * @return {{plugins: [import('postcss').PluginCreator<any>, false | Record<string, any> | undefined][]}}
+ */
 module.exports = function defaultPreset(opts = {}) {
   const options = Object.assign({}, defaultOpts, opts);
-
+  /** @type {[import('postcss').PluginCreator<any>, false | Record<string, any> | undefined][]} **/
   const plugins = [
     [postcssDiscardComments, options.discardComments],
     [postcssNormalizeWhitespace, options.normalizeWhitespace],
