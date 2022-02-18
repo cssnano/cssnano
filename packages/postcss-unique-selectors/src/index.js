dc6c4428fcc49d770ff2bd65f798627c39de6566
Ludovico Fischer <livrerie@gmail.com>
2022-02-18 20:23:51 +0100

    feat(postcss-unique-selectors): add types

--- packages/postcss-unique-selectors/src/index.js
+++ packages/postcss-unique-selectors/src/index.js
@@ -1,21 +1,35 @@
 'use strict';
 const selectorParser = require('postcss-selector-parser');
 
+/**
+ * @param {string} selectors
+ * @param {selectorParser.SyncProcessor<void>} callback
+ * @return {string}
+ */
 function parseSelectors(selectors, callback) {
   return selectorParser(callback).processSync(selectors);
 }
 
+/**
+ * @param {import('postcss').Rule} rule
+ * @return {void}
+ */
 function unique(rule) {
   const selector = [...new Set(rule.selectors)];
   selector.sort();
   rule.selector = selector.join();
 }
 
+/**
+ * @type {import('postcss').PluginCreator<void>}
+ * @return {import('postcss').Plugin}
+ */
 function pluginCreator() {
   return {
     postcssPlugin: 'postcss-unique-selectors',
     OnceExit(css) {
       css.walkRules((nodes) => {
+        /** @type {string[]} */
         let comments = [];
         nodes.selector = parseSelectors(nodes.selector, (selNode) => {
           selNode.walk((sel) => {
