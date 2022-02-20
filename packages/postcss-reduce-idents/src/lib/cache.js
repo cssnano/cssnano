c327a3d8b490f9f3dfb464b9a23357d820a837c1
Ludovico Fischer <livrerie@gmail.com>
2022-02-20 23:55:22 +0100

    feat(postcss-reduce-idents): add types

--- packages/postcss-reduce-idents/src/lib/cache.js
+++ packages/postcss-reduce-idents/src/lib/cache.js
@@ -1,4 +1,10 @@
 'use strict';
+/**
+ * @param {string} value
+ * @param {(value: string, num: number) => string} encoder
+ * @param {Record<string, {ident: string, count: number}>} cache
+ * @return {void}
+ */
 module.exports = function (value, encoder, cache) {
   if (cache[value]) {
     return;
--- packages/postcss-reduce-idents/src/lib/encode.js
+++ packages/postcss-reduce-idents/src/lib/encode.js
@@ -1,4 +1,9 @@
 'use strict';
+/**
+ * @param {string} val
+ * @param {number} num
+ * @return {string}
+ */
 module.exports = function encode(val, num) {
   let base = 52;
   let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
