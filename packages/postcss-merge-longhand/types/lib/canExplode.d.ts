48f03978771be96c40ce9938c7d41da4822ecb3d
Ludovico Fischer <livrerie@gmail.com>
2022-06-19 20:09:55 +0200

    chore: update TypeScript and re-generate types

--- packages/postcss-merge-longhand/types/lib/canExplode.d.ts
+++ packages/postcss-merge-longhand/types/lib/canExplode.d.ts
@@ -1,2 +1,2 @@
-declare const _exports: (prop: import('postcss').Declaration, includeCustomProps?: boolean | undefined) => boolean;
+declare const _exports: (prop: import('postcss').Declaration, includeCustomProps?: boolean) => boolean;
 export = _exports;
