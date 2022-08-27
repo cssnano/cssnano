786bb2dc6ce836c5df0983f48ac0391642d03a0e
Ludovico Fischer <livrerie@gmail.com>
2022-08-27 07:34:07 +0200

    chore: update TypeScript to 4.8.2

--- packages/postcss-merge-longhand/types/lib/colornames.d.ts
+++ packages/postcss-merge-longhand/types/lib/colornames.d.ts
@@ -1,14 +1,2 @@
-declare const _exports: {
-    add(value: string): Set<string>;
-    clear(): void;
-    delete(value: string): boolean;
-    forEach(callbackfn: (value: string, value2: string, set: Set<string>) => void, thisArg?: any): void;
-    has(value: string): boolean;
-    readonly size: number;
-    entries(): IterableIterator<[string, string]>;
-    keys(): IterableIterator<string>;
-    values(): IterableIterator<string>;
-    [Symbol.iterator](): IterableIterator<string>;
-    readonly [Symbol.toStringTag]: string;
-};
+declare const _exports: Set<string>;
 export = _exports;
--- packages/postcss-normalize-repeat-style/types/lib/map.d.ts
+++ packages/postcss-normalize-repeat-style/types/lib/map.d.ts
@@ -1,15 +1,2 @@
-declare const _exports: {
-    clear(): void;
-    delete(key: string): boolean;
-    forEach(callbackfn: (value: string, key: string, map: Map<string, string>) => void, thisArg?: any): void;
-    get(key: string): string | undefined;
-    has(key: string): boolean;
-    set(key: string, value: string): Map<string, string>;
-    readonly size: number;
-    entries(): IterableIterator<[string, string]>;
-    keys(): IterableIterator<string>;
-    values(): IterableIterator<string>;
-    [Symbol.iterator](): IterableIterator<[string, string]>;
-    readonly [Symbol.toStringTag]: string;
-};
+declare const _exports: Map<string, string>;
 export = _exports;
