91a7cdb0b9ffa1e7dc1f2339a32fc08f949f4155
Ludovico Fischer <43557+ludofischer@users.noreply.github.com>
2023-03-23 13:41:18 +0100

    fix(postcss-reduce-initial): ensure options are always read (#1482)
    Fix #1436
    
    Ensure options are read also when the plugin is configured
    in cssnano instead as a standalone PostCSS plugin.

--- packages/postcss-reduce-initial/types/index.d.ts
+++ packages/postcss-reduce-initial/types/index.d.ts
@@ -1,9 +1,15 @@
 export = pluginCreator;
+/** @typedef {{ignore?: string[]}} Options */
 /**
- * @type {import('postcss').PluginCreator<void>}
+ * @type {import('postcss').PluginCreator<Options>}
+ * @param {Options} options
  * @return {import('postcss').Plugin}
  */
-declare function pluginCreator(): import('postcss').Plugin;
+declare function pluginCreator(options?: Options): import('postcss').Plugin;
 declare namespace pluginCreator {
-    const postcss: true;
+    export { postcss, Options };
 }
+type Options = {
+    ignore?: string[];
+};
+declare var postcss: true;
