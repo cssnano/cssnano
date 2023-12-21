01125672ea19a6b9296aa880c35964714a8bc0e5
Ludovico Fischer <livrerie@gmail.com>
2023-12-21 21:23:13 +0100

    test(cssnano): update google-fonts-complete dependency

--- packages/cssnano/test/issue420.js
+++ packages/cssnano/test/issue420.js
@@ -17,7 +17,7 @@ test('should work with postcss-font-magician with `display` parameter', () => {
     .then((result) => {
       assert.snapshot(
         result.css,
-        `@font-face{font-display:optional;font-family:Alice;font-style:normal;font-weight:400;src:local("Alice Regular"),local(Alice-Regular),url(//fonts.gstatic.com/s/alice/v12/OpNCnoEEmtHa6GcOrgo.eot?#) format("eot"),url(//fonts.gstatic.com/s/alice/v12/OpNCnoEEmtHa6GcOrg4.woff2) format("woff2"),url(//fonts.gstatic.com/s/alice/v12/OpNCnoEEmtHa6GcOrgg.woff) format("woff")}body{font-family:Alice}`
+        `@font-face{font-display:optional;font-family:Alice;font-style:normal;font-weight:400;src:url(//fonts.gstatic.com/s/alice/v20/OpNCnoEEmtHa6GcArgo.eot?#) format("eot"),url(//fonts.gstatic.com/s/alice/v20/OpNCnoEEmtHa6GcOrg4.woff2) format("woff2"),url(//fonts.gstatic.com/s/alice/v20/OpNCnoEEmtHa6GcArgg.woff) format("woff")}body{font-family:Alice}`
       );
     });
 });
