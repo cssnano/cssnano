ca06b87b835fb72f2108bd667e8f81224e246741
Colin Rotherham <work@colinr.com>
2024-03-02 22:56:46 +0000

    fix: use type definition properties for cssnano presets

--- packages/cssnano-preset-lite/src/index.js
+++ packages/cssnano-preset-lite/src/index.js
@@ -4,20 +4,32 @@ const postcssNormalizeWhitespace = require('postcss-normalize-whitespace');
 const postcssDiscardEmpty = require('postcss-discard-empty');
 const { rawCache } = require('cssnano-utils');
 
-/** @typedef {false | { exclude?: true} }SimpleOptions */
-/** @typedef {{discardComments?: false | import('postcss-discard-comments').Options & { exclude?: true},
-normalizeWhitespace?: SimpleOptions, 
-discardEmpty?: SimpleOptions, 
-rawCache?: SimpleOptions}} Options */
-
-const defaultOpts = {};
 /**
- * @param {Options} opts
- * @return {{plugins: [import('postcss').PluginCreator<any>, false | Record<string, any> | undefined][]}}
+ * @template {object | void} [OptionsExtends=void]
+ * @typedef {false | OptionsExtends & {exclude?: true}} SimpleOptions
  */
-function defaultPreset(opts = {}) {
+
+/**
+ * @typedef {object} LiteOptions
+ * @property {SimpleOptions<import('postcss-discard-comments').Options>} [discardComments]
+ * @property {SimpleOptions} [normalizeWhitespace]
+ * @property {SimpleOptions} [discardEmpty]
+ * @property {SimpleOptions} [rawCache]
+ */
+
+/** @satisfies {LiteOptions} */
+const defaultOpts = {};
+
+/**
+ * Safe and minimum transformation with just removing whitespaces, line breaks and comments
+ *
+ * @param {LiteOptions} opts
+ * @returns {{ plugins: [import('postcss').PluginCreator<any>, LiteOptions[keyof LiteOptions]][] }}
+ */
+function litePreset(opts = {}) {
   const options = Object.assign({}, defaultOpts, opts);
-  /** @type {[import('postcss').PluginCreator<any>, false | Record<string, any> | undefined][]} **/
+
+  /** @satisfies {ReturnType<typeof litePreset>["plugins"]} */
   const plugins = [
     [postcssDiscardComments, options.discardComments],
     [postcssNormalizeWhitespace, options.normalizeWhitespace],
@@ -28,4 +40,4 @@ function defaultPreset(opts = {}) {
   return { plugins };
 }
 
-module.exports = defaultPreset;
+module.exports = litePreset;
