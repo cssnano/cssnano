9fce0a855225f4ab6d08c24b684a872b041c7351
ota-meshi <otameshiyo23@gmail.com>
2024-06-10 09:22:20 +0900

    fix(postcss-ordered-values): incorrect order when using calc for animation property

---
+++ packages/cssnano/test/issue1527.js
@@ -0,0 +1,26 @@
+'use strict';
+const { test } = require('node:test');
+const assert = require('node:assert/strict');
+const postcss = require('postcss');
+const preset = require('cssnano-preset-default');
+const nano = require('..');
+
+const fixture = `
+.b {
+  animation: opacity 0ms calc(1000ms);
+}
+`;
+
+const expected = '.b{animation:opacity 0ms calc(1s)}';
+
+test('it should keep quote', () => {
+  const processor = postcss([
+    nano({
+      preset: preset({ calc: false }),
+    }),
+  ]);
+
+  return processor
+    .process(fixture, { from: undefined })
+    .then((r) => assert.strictEqual(r.css, expected));
+});
