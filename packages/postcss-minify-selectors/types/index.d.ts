4fbbc6edc710251c1704dac59dcffe0b81731718
Dmitry Kryaklin <dmkryak@gmail.com>
2026-04-20 16:56:52 +0300

    feat(postcss-minify-selectors): fold selector lists into :is() (#1703) (#1775)

--- packages/postcss-minify-selectors/types/index.d.ts
+++ packages/postcss-minify-selectors/types/index.d.ts
@@ -1,18 +1,41 @@
 export = pluginCreator;
-/** @typedef {object} Options
- *  @property {boolean} [sort=true]
+/**
+ * @typedef {{ overrideBrowserslist?: string | string[] }} AutoprefixerOptions
+ * @typedef {Pick<browserslist.Options, 'stats' | 'path' | 'env'>} BrowserslistOptions
  */
 /**
- * @type {import('postcss').PluginCreator<void>}
+ * @typedef {object} OwnOptions
+ * @property {boolean} [sort=true]
+ * @property {boolean} [convertToIs=true] Factor shared prefixes/suffixes in a
+ *   comma-separated selector list into `:is(...)` when it produces shorter
+ *   output and is safe with respect to cascade specificity. Automatically
+ *   skipped when the configured browserslist target doesn't support `:is()`.
+ */
+/** @typedef {OwnOptions & AutoprefixerOptions & BrowserslistOptions} Options */
+/**
+ * @type {import('postcss').PluginCreator<Options>}
  * @param {Options} opts
  * @return {import('postcss').Plugin}
  */
-declare function pluginCreator(opts?: Options): import("postcss").Plugin;
+declare function pluginCreator(opts: Options): import("postcss").Plugin;
 declare namespace pluginCreator {
-    export { postcss, Options };
+    export { postcss, AutoprefixerOptions, BrowserslistOptions, OwnOptions, Options };
 }
 declare var postcss: true;
-type Options = {
-    sort?: boolean | undefined;
+type AutoprefixerOptions = {
+    overrideBrowserslist?: string | string[];
 };
+type BrowserslistOptions = Pick<browserslist.Options, "stats" | "path" | "env">;
+type OwnOptions = {
+    sort?: boolean | undefined;
+    /**
+     * Factor shared prefixes/suffixes in a
+     * comma-separated selector list into `:is(...)` when it produces shorter
+     * output and is safe with respect to cascade specificity. Automatically
+     * skipped when the configured browserslist target doesn't support `:is()`.
+     */
+    convertToIs?: boolean | undefined;
+};
+type Options = OwnOptions & AutoprefixerOptions & BrowserslistOptions;
+import browserslist = require("browserslist");
 //# sourceMappingURL=index.d.ts.map
