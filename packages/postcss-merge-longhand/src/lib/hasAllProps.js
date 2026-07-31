ca2c00628f587275ec55b8ccafc58720a7145043
Ludovico Fischer <livrerie@gmail.com>
2026-07-31 13:39:18 +0200

    chore: improve function documentation

--- packages/postcss-merge-longhand/src/lib/hasAllProps.js
+++ packages/postcss-merge-longhand/src/lib/hasAllProps.js
@@ -1,5 +1,11 @@
 'use strict';
-/** @type {(rule: import('postcss').Declaration[], ...props: string[]) => boolean} */
+/**
+ *  Returns whether the rule contains every property.
+ *
+ * @type {(rule: import('postcss').Declaration[], ...props: string[]) => boolean}
+ *
+ * @returns {boolean}
+ * */
 module.exports = (rule, ...props) => {
   return props.every((p) =>
     rule.some((node) => node.prop && node.prop.toLowerCase().includes(p))
