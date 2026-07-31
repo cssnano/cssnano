b570b0b29a4a8e040b3093c2c854b43e5ac6f2f1
Ludovico Fischer <livrerie@gmail.com>
2026-07-31 12:05:46 +0200

    chore: return boolean expression directly

--- packages/postcss-merge-longhand/src/lib/canExplode.js
+++ packages/postcss-merge-longhand/src/lib/canExplode.js
@@ -5,12 +5,9 @@ const globalKeywords = new Set(['inherit', 'initial', 'unset', 'revert']);
 
 /** @type {(prop: import('postcss').Declaration, includeCustomProps?: boolean) => boolean} */
 module.exports = (prop, includeCustomProps = true) => {
-  if (
+  return !(
     !prop.value ||
     (includeCustomProps && isCustomProp(prop)) ||
     (prop.value && globalKeywords.has(prop.value.toLowerCase()))
-  ) {
-    return false;
-  }
-  return true;
+  );
 };
