22ee1d45b8f7b27d1fa3c60c86816eb551fdf97c
Ludovico Fischer <livrerie@gmail.com>
2026-08-01 08:58:18 +0200

    test(postcss-discard-overridden): adopt same test harness as other packages

--- packages/postcss-discard-overridden/test/index.js
+++ packages/postcss-discard-overridden/test/index.js
@@ -1,72 +1,234 @@
 'use strict';
-const fs = require('node:fs');
-const assert = require('node:assert/strict');
 const { test } = require('node:test');
-const postcss = require('postcss');
-const { diffLines } = require('diff');
-const pc = require('picocolors');
 const plugin = require('../src/index.js');
+const { processCSSFactory } = require('../../../util/testHelpers.js');
+const { processCSS } = processCSSFactory(plugin);
 
-function getDiff(left, right) {
-  const msg = ['\n'];
-
-  diffLines(left, right).forEach((item) => {
-    if (item.added || item.removed) {
-      const text = item.value
-        .replace('\n', '\u00b6\n')
-        .replace('\ufeff', '[[BOM]]');
-
-      msg.push(pc[item.added ? 'green' : 'red'](text));
-    } else {
-      const value = item.value.replace('\ufeff', '[[BOM]]');
-      const lines = value.split('\n');
-
-      // max line count for each item
-      const keepLines = 6;
-      // lines to be omitted
-      const omitLines = lines.length - keepLines;
-
-      if (lines.length > keepLines) {
-        lines.splice(
-          Math.floor(keepLines / 2),
-          omitLines,
-          pc.gray('(...' + omitLines + ' lines omitted...)')
-        );
-      }
-
-      msg.concat(lines);
+test(
+  'overridden @keyframes should be discarded correctly',
+  processCSS(
+    `@-webkit-keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0.8;
+  }
+}
+@-WEBKIT-KEYFRAMES fade-in {
+    0% {
+        opacity: 0;
     }
-  });
-
-  msg.push('\n');
-
-  return msg.map((line) => '  ' + line).join('');
+    100% {
+        opacity: 0.8;
+    }
 }
-
-function read(file) {
-  return fs.readFileSync(__dirname + `/fixtures/${file}.css`, {
-    encoding: 'utf-8',
-  });
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0.8;
+  }
 }
-
-function exec(input) {
-  const output = read(`${input}.post`);
-
-  return () =>
-    postcss([plugin()])
-      .process(read(input), { from: undefined })
-      .then((result) => {
-        if (result.css !== output) {
-          throw getDiff(result.css, output);
-        }
-
-        assert.strictEqual(result.warnings().length, 0);
-      });
+@KEYFRAMES fade-in {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 0.8;
+    }
 }
-
-test('overridden @keyframes should be discarded correctly', exec('keyframes'));
+@media (max-width: 500px) {
+  @-webkit-keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  @-webkit-keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0.8;
+    }
+  }
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0.8;
+    }
+  }
+  @supports (display: flex) {
+    @-webkit-keyframes fade-in {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+    @keyframes fade-in {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+  }
+}
+@-webkit-keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+`,
+    `@media (max-width: 500px) {
+  @-webkit-keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0.8;
+    }
+  }
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0.8;
+    }
+  }
+  @supports (display: flex) {
+    @-webkit-keyframes fade-in {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+    @keyframes fade-in {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+  }
+}
+@-webkit-keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+`
+  )
+);
 
 test(
   'overridden @counter-style should be discarded correctly',
-  exec('counter-style')
+  processCSS(
+    `@counter-style my-alpha {
+  system: fixed;
+  symbols: A B C;
+  suffix: " ";
+}
+
+@COUNTER-STYLE my-alpha {
+    system: fixed;
+    symbols: A B C;
+    suffix: " ";
+}
+
+@counter-style my-alpha {
+  system: fixed;
+  symbols: Ⓐ Ⓑ Ⓒ;
+  suffix: " ";
+}
+
+@media (max-width: 400px) {
+  @counter-style my-alpha {
+    system: fixed;
+    symbols: A B C;
+    suffix: " ";
+  }
+
+  @supports (display: flex) {
+    @counter-style my-alpha {
+      system: fixed;
+      symbols: a b c;
+      suffix: " ";
+    }
+  }
+
+  @counter-style my-alpha {
+    system: fixed;
+    symbols: Ⓐ Ⓑ Ⓒ;
+    suffix: " ";
+  }
+}
+`,
+    `@counter-style my-alpha {
+  system: fixed;
+  symbols: Ⓐ Ⓑ Ⓒ;
+  suffix: " ";
+}
+
+@media (max-width: 400px) {
+
+  @supports (display: flex) {
+    @counter-style my-alpha {
+      system: fixed;
+      symbols: a b c;
+      suffix: " ";
+    }
+  }
+
+  @counter-style my-alpha {
+    system: fixed;
+    symbols: Ⓐ Ⓑ Ⓒ;
+    suffix: " ";
+  }
+}
+`
+  )
 );
