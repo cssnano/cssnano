e1a74198cb3a43bab44a9432ecdbc17adc71c181
Ludovico Fischer <livrerie@gmail.com>
2026-08-05 02:19:59 +0200

    fix(postcss-svgo): improve character encoding

---
+++ .changeset/every-teeth-stay.md
@@ -0,0 +1,8 @@
+---
+"postcss-svgo": patch
+"cssnano": patch
+"cssnano-preset-advanced": patch
+"cssnano-preset-default": patch
+---
+
+fix(postcss-svgo): encode chracters in the correct order
--- packages/postcss-svgo/src/lib/url.js
+++ packages/postcss-svgo/src/lib/url.js
@@ -5,15 +5,15 @@
  */
 function encode(data) {
   return data
-    .replace(/'/g, '&apos;')
-    .replace(/"/g, "'")
     .replace(/%/g, '%25')
+    .replace(/&/g, '%26')
+    .replace(/'/g, '%22')
+    .replace(/"/g, "'")
     .replace(/</g, '%3C')
     .replace(/>/g, '%3E')
-    .replace(/&/g, '%26')
     .replace(/#/g, '%23')
     .replace(/\s+/g, ' ');
 }
 
 const decode = decodeURIComponent;
-module.exports = { encode, decode };
+module.exports = { encode , decode };
