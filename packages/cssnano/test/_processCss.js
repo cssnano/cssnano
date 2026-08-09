8697a499af65b3eabd0c1d2b1d0efdaeec15f973
Ludovico Fischer <livrerie@gmail.com>
2026-08-09 13:44:49 +0200

    test: reuse PostCSS instance when config does not change

--- packages/cssnano/test/_processCss.js
+++ packages/cssnano/test/_processCss.js
@@ -3,13 +3,13 @@ const assert = require('node:assert/strict');
 const postcss = require('postcss');
 const cssnano = require('../src/index.js');
 
+const processor = postcss([cssnano()]);
+
 function processCss(fixture, expected, options = { from: undefined }) {
   return () =>
-    postcss([cssnano()])
-      .process(fixture, options)
-      .then(({ css }) => {
-        assert.strictEqual(css, expected);
-      });
+    processor.process(fixture, options).then(({ css }) => {
+      assert.strictEqual(css, expected);
+    });
 }
 module.exports = processCss;
 module.exports.passthrough = function (fixture, options = { from: undefined }) {
