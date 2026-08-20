f9c2ba2e9218170dae6b6073b5fa747b09bce4d8
Ludovico Fischer <livrerie@gmail.com>
2026-08-20 12:08:08 +0200

    test: reorganize integration tests
    test: avoid too much nesting and allow testing site separately

--- packages/cssnano-preset-default/test/integrations.js
+++ packages/cssnano-preset-default/test/integrations.js
@@ -1,6 +1,6 @@
 'use strict';
 const { join } = require('node:path');
-const { test } = require('node:test');
+const { describe, test } = require('node:test');
 const assert = require('node:assert/strict');
 const {
   integrationTests,
@@ -17,34 +17,35 @@ const withBrowserslist = processCSSWithPresetFactory(
   })
 );
 
-test(
-  'should process CSS with default options',
-  withDefaults.processCSS(
-    'button { color: hsla(0 100% 50% / 40%); appearance: none }',
-    'button{color:rgba(255,0,0,.4);appearance:none}'
-  )
-);
+describe('CSS processing', () => {
+  test(
+    'should process CSS with default options',
+    withDefaults.processCSS(
+      'button { color: hsla(0 100% 50% / 40%); appearance: none }',
+      'button{color:rgba(255,0,0,.4);appearance:none}'
+    )
+  );
 
-test(
-  'should process CSS with Browserslist options',
-  withBrowserslist.processCSS(
-    'button { color: hsla(0 100% 50% / 40%); appearance: none }',
-    'button{color:#f006;appearance:none}'
-  )
-);
+  test(
+    'should process CSS with Browserslist options',
+    withBrowserslist.processCSS(
+      'button { color: hsla(0 100% 50% / 40%); appearance: none }',
+      'button{color:#f006;appearance:none}'
+    )
+  );
 
-test(
-  'should keep viewBox attribute in SVG from removing',
-  withDefaults.processCSS(
-    `a { background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="200" height="100" viewBox="0 0 200 100"><rect width="100" height="100" /></svg>'); }`,
-    `a{background-image:url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="200" height="100" viewBox="0 0 200 100"><path d="M0 0h100v100H0z"/></svg>')}`
-  )
-);
+  test(
+    'should keep viewBox attribute in SVG from removing',
+    withDefaults.processCSS(
+      `a { background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="200" height="100" viewBox="0 0 200 100"><rect width="100" height="100" /></svg>'); }`,
+      `a{background-image:url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="200" height="100" viewBox="0 0 200 100"><path d="M0 0h100v100H0z"/></svg>')}`
+    )
+  );
 
-test(
-  "should merge rules sharing identical declarations without dropping the trailing rule's unique ones",
-  withDefaults.processCSS(
-    `.has-errors .input {
+  test(
+    "should merge rules sharing identical declarations without dropping the trailing rule's unique ones",
+    withDefaults.processCSS(
+      `.has-errors .input {
   background-color: #fcebea;
   border-color: #cc1f1a;
 }
@@ -75,32 +76,37 @@ test(
   line-height: 1.5;
   margin-bottom: .5rem;
 }`,
-    '.error-banner .field-errors.filled,.has-errors .checkbox-inline,.has-errors .checkbox-label,.has-errors .input{background-color:#fcebea;border-color:#cc1f1a}.error-banner .field-errors.filled{width:100%;padding:1.5rem 1.5rem 1rem;border-bottom-width:1px;border-style:solid}.error-banner .field-errors.filled .field-error{width:100%;font-size:.875rem;color:#22292f;line-height:1.5;margin-bottom:.5rem}'
-  )
-);
-
-test(
-  'should correctly handle the framework tests',
-  { concurrency: true },
-  integrationTests(preset, `${__dirname}/integrations`)
-);
-
-test('preserves color values when colormin is disabled', async () => {
-  const input = 'h1{color:black}';
-  const { css } = await createCssnanoProcessor(
-    preset({ colormin: false })
-  ).process(input, {
-    from: undefined,
-  });
-
-  assert.strictEqual(css, input);
+      '.error-banner .field-errors.filled,.has-errors .checkbox-inline,.has-errors .checkbox-label,.has-errors .input{background-color:#fcebea;border-color:#cc1f1a}.error-banner .field-errors.filled{width:100%;padding:1.5rem 1.5rem 1rem;border-bottom-width:1px;border-style:solid}.error-banner .field-errors.filled .field-error{width:100%;font-size:.875rem;color:#22292f;line-height:1.5;margin-bottom:.5rem}'
+    )
+  );
 });
 
-test('preserves color values with the exclude option', async () => {
-  const input = 'h1{color:black}';
-  const { css } = await createCssnanoProcessor(
-    preset({ colormin: { exclude: true } })
-  ).process(input, { from: undefined });
+describe('framework integrations', () => {
+  test(
+    'should correctly handle the framework tests',
+    { concurrency: true },
+    integrationTests(preset, `${__dirname}/integrations`)
+  );
+});
 
-  assert.strictEqual(css, input);
+describe('colormin options', () => {
+  test('preserves color values when colormin is disabled', async () => {
+    const input = 'h1{color:black}';
+    const { css } = await createCssnanoProcessor(
+      preset({ colormin: false })
+    ).process(input, {
+      from: undefined,
+    });
+
+    assert.strictEqual(css, input);
+  });
+
+  test('preserves color values with the exclude option', async () => {
+    const input = 'h1{color:black}';
+    const { css } = await createCssnanoProcessor(
+      preset({ colormin: { exclude: true } })
+    ).process(input, { from: undefined });
+
+    assert.strictEqual(css, input);
+  });
 });
