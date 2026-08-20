6bd7b64462ab6dfe29ff4286c495d12ef87c5cc6
Ludovico Fischer <livrerie@gmail.com>
2026-08-20 12:08:08 +0200

    test: reorganize integration tests
    test: avoid too much nesting and allow testing site separately

--- packages/cssnano-preset-advanced/test/integrations.js
+++ packages/cssnano-preset-advanced/test/integrations.js
@@ -1,6 +1,6 @@
 'use strict';
 const { join } = require('node:path');
-const { test } = require('node:test');
+const { describe, test } = require('node:test');
 const assert = require('node:assert/strict');
 const {
   integrationTests,
@@ -23,44 +23,50 @@ const withBrowserslist = processCSSWithPresetFactory(
   })
 );
 
-test(
-  'should process CSS with default options',
-  withDefaults.processCSS(
-    'button { color: hsla(0 100% 50% / 40%); appearance: none }',
-    'button{appearance:none;color:rgba(255,0,0,.4)}'
-  )
-);
+describe('CSS processing', () => {
+  test(
+    'should process CSS with default options',
+    withDefaults.processCSS(
+      'button { color: hsla(0 100% 50% / 40%); appearance: none }',
+      'button{appearance:none;color:rgba(255,0,0,.4)}'
+    )
+  );
 
-test(
-  'should process CSS with Browserslist options',
-  withBrowserslist.processCSS(
-    'button { color: hsla(0 100% 50% / 40%); appearance: none }',
-    'button{-webkit-appearance:none;-moz-appearance:none;appearance:none;color:#f006}'
-  )
-);
-
-test(
-  'should correctly handle the framework tests',
-  { concurrency: true },
-  integrationTests(preset, `${__dirname}/integrations`)
-);
-
-test('preserves z-index values when zindex is disabled', async () => {
-  const input = 'h1{z-index:10}';
-  const { css } = await createCssnanoProcessor(
-    preset({ zindex: false })
-  ).process(input, {
-    from: undefined,
-  });
-
-  assert.strictEqual(css, input);
+  test(
+    'should process CSS with Browserslist options',
+    withBrowserslist.processCSS(
+      'button { color: hsla(0 100% 50% / 40%); appearance: none }',
+      'button{-webkit-appearance:none;-moz-appearance:none;appearance:none;color:#f006}'
+    )
+  );
 });
 
-test('preserves z-index values with the exclude option', async () => {
-  const input = 'h1{z-index:10}';
-  const { css } = await createCssnanoProcessor(
-    preset({ zindex: { exclude: true } })
-  ).process(input, { from: undefined });
+describe('framework integrations', () => {
+  test(
+    'should correctly handle the framework tests',
+    { concurrency: true },
+    integrationTests(preset, `${__dirname}/integrations`)
+  );
+});
 
-  assert.strictEqual(css, input);
+describe('z-index options', () => {
+  test('preserves z-index values when zindex is disabled', async () => {
+    const input = 'h1{z-index:10}';
+    const { css } = await createCssnanoProcessor(
+      preset({ zindex: false })
+    ).process(input, {
+      from: undefined,
+    });
+
+    assert.strictEqual(css, input);
+  });
+
+  test('preserves z-index values with the exclude option', async () => {
+    const input = 'h1{z-index:10}';
+    const { css } = await createCssnanoProcessor(
+      preset({ zindex: { exclude: true } })
+    ).process(input, { from: undefined });
+
+    assert.strictEqual(css, input);
+  });
 });
--- packages/cssnano-preset-lite/test/integrations.js
+++ packages/cssnano-preset-lite/test/integrations.js
@@ -1,10 +1,12 @@
 'use strict';
-const { test } = require('node:test');
+const { describe, test } = require('node:test');
 const { integrationTests } = require('../../../util/integrationTestHelpers.js');
 const preset = require('../src/index.js');
 
-test(
-  'should correctly handle the framework tests',
-  { concurrency: true },
-  integrationTests(preset, `${__dirname}/integrations`)
-);
+describe('lite preset framework integrations', () => {
+  test(
+    'should correctly handle the framework tests',
+    { concurrency: true },
+    integrationTests(preset, `${__dirname}/integrations`)
+  );
+});
