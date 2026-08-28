41f5b62afb2d5d3e558c82373e2b6c5e89b4a596
Ludovico Fischer <livrerie@gmail.com>
2026-08-28 10:13:30 +0200

    perf(stylehacks): cache detection result

--- packages/stylehacks/src/index.js
+++ packages/stylehacks/src/index.js
@@ -1,6 +1,8 @@
 import getBrowsersList from '#getBrowsersList';
 import plugins from './plugins/index.js';
 
+/** @import {Declaration} from 'postcss'; */
+
 /**
  * @import browserslist from 'browserslist'
  * @typedef {{ overrideBrowserslist?: string | string[] }} AutoprefixerOptions
@@ -56,14 +58,48 @@ function pluginCreator(opts = {}) {
   };
 }
 
-/** @type {(node: import('postcss').Node) => boolean} */
-pluginCreator.detect = (node) => {
+/** @type {WeakMap<Declaration, {before: string | undefined, prop: string, value: string, detected: boolean}>} */
+const declarationDetections = new WeakMap();
+
+/** @param {import('postcss').Node} node */
+function detect(node) {
+  if (node.type === 'decl') {
+    const declaration = /** @type {Declaration} */ (node);
+    const { before } = declaration.raws;
+    const cached = declarationDetections.get(declaration);
+
+    if (
+      cached &&
+      cached.before === before &&
+      cached.prop === declaration.prop &&
+      cached.value === declaration.value
+    ) {
+      return cached.detected;
+    }
+
+    const detected = plugins.some((Plugin) => {
+      const hack = new Plugin();
+
+      return hack.any(node);
+    });
+    declarationDetections.set(declaration, {
+      before,
+      prop: declaration.prop,
+      value: declaration.value,
+      detected,
+    });
+
+    return detected;
+  }
+
   return plugins.some((Plugin) => {
     const hack = new Plugin();
 
     return hack.any(node);
   });
-};
+}
+
+pluginCreator.detect = detect;
 
 pluginCreator.postcss = true;
 const moduleExports =
