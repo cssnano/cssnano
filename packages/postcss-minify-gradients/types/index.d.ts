d526769360d9ed2ead7843229f9a76fb11e71813
Ludovico Fischer <livrerie@gmail.com>
2026-09-02 11:20:10 +0200

    refactor(postcss-minify-gradients): replace parser helpers with tokenizer utilities

--- packages/postcss-minify-gradients/types/index.d.ts
+++ packages/postcss-minify-gradients/types/index.d.ts
@@ -1,10 +1,13 @@
-/**
- * @return {import('postcss').Plugin}
- */
+import cssnanoUtils from 'cssnano-utils';
+/** @type {typeof cssnanoUtils.balancedTokens} */
+declare const balancedTokens: typeof cssnanoUtils.balancedTokens;
+export type CSSToken = ReturnType<typeof balancedTokens> extends infer Structure ? Structure extends {
+    tokens: readonly (infer Token)[];
+} ? Token : never : never;
+/** @return {import('postcss').Plugin} */
 declare function pluginCreator(): import('postcss').Plugin;
 declare namespace pluginCreator {
     var postcss: true;
 }
-declare const moduleExports: typeof pluginCreator;
-export { moduleExports as default, moduleExports as 'module.exports' };
+export { pluginCreator as default, pluginCreator as 'module.exports' };
 //# sourceMappingURL=index.d.ts.map
