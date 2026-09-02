c2818ddf23958edc46da81246b5ef75db0e83598
Ludovico Fischer <livrerie@gmail.com>
2026-09-02 11:20:10 +0200

    refactor(stylehacks): replace selector parser with tokenizer utilities

--- packages/stylehacks/types/exists.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @param {import('postcss-selector-parser').Selector} selector
- * @param {number} index
- * @param {string} value
- * @return {boolean | undefined | ''}
- */
-declare function exists(selector: import('postcss-selector-parser').Selector, index: number, value: string): boolean | undefined | '';
-export default exists;
-//# sourceMappingURL=exists.d.ts.map
