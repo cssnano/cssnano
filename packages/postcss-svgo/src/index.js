495c1a98877428aa64212c54b6f6f69f248dc159
Ludovico Fischer <livrerie@gmail.com>
2026-09-02 11:20:10 +0200

    refactor(postcss-svgo): replace parser helpers with tokenizer utilities

--- packages/postcss-svgo/src/index.js
+++ packages/postcss-svgo/src/index.js
@@ -1,9 +1,10 @@
-import valueParser from 'postcss-value-parser';
+import { tokenize, TokenType } from '@csstools/css-tokenizer';
 import { optimize } from 'svgo';
 import { encode, decode } from './lib/url.js';
 
+/** @import {CSSToken} from '@csstools/css-tokenizer' */
 const PLUGIN = 'postcss-svgo';
-const dataURI = /data:image\/svg\+xml(;((charset=)?utf-8|base64))?,/i;
+const dataURI = /data:image\/svg\+xml(?:;(?:(?:charset=)?(?:utf-8|base64)))?,/i;
 const dataURIBase64 = /data:image\/svg\+xml;base64,/i;
 
 // the following regex will globally match:
@@ -49,6 +50,26 @@ function minifySVG(input, opts) {
   };
 }
 
+/** @param {string} value @param {Options} opts @return {{value: string, quote: string} | undefined} */
+function optimizeDataUri(value, opts) {
+  if (dataURIBase64.test(value)) {
+    const url = new URL(value);
+    const base64String = `${url.protocol}${url.pathname}`.replace(dataURI, '');
+    const svg = Buffer.from(base64String, 'base64').toString('utf8');
+    const { result } = minifySVG(svg, opts);
+    const data = Buffer.from(result).toString('base64');
+    return { value: 'data:image/svg+xml;base64,' + data + url.hash, quote: '' };
+  }
+  if (!dataURI.test(value)) return undefined;
+  const svg = value.replace(dataURI, '');
+  const { result, isUriEncoded } = minifySVG(svg, opts);
+  const data = (isUriEncoded ? encode(result) : result).replace(/#/g, '%23');
+  return {
+    value: 'data:image/svg+xml;charset=utf-8,' + data,
+    quote: isUriEncoded ? '"' : "'",
+  };
+}
+
 /**
  * @param {import('postcss').Declaration} decl
  * @param {Options} opts
@@ -56,62 +77,65 @@ function minifySVG(input, opts) {
  * @return {void}
  */
 function minify(decl, opts, postcssResult) {
-  const parsed = valueParser(decl.value);
-
-  const minified = parsed.walk((node) => {
+  const original = decl.value;
+  /** @type {CSSToken[]} */ const tokens = [...tokenize({ css: original })];
+  /** @type {[number, number, string][]} */ const replacements = [];
+  for (let i = 0; i < tokens.length; i++) {
+    const functionToken = tokens[i];
+    let close = i + 1;
+    let prefix;
+    let value;
+    let quote;
     if (
-      node.type !== 'function' ||
-      node.value.toLowerCase() !== 'url' ||
-      !node.nodes.length
+      functionToken[0] === TokenType.URL ||
+      functionToken[0] === TokenType.BadURL
     ) {
-      return;
+      prefix = functionToken[1].slice(0, functionToken[1].indexOf('(') + 1);
+      value =
+        functionToken[4]?.value ?? functionToken[1].slice(prefix.length, -1);
+      quote = '';
+      close = i + 1;
+    } else {
+      if (
+        functionToken[0] !== TokenType.Function ||
+        functionToken[1].slice(0, -1).toLowerCase() !== 'url'
+      )
+        continue;
+      let depth = 1;
+      for (; close < tokens.length && depth; close++) {
+        if (tokens[close][0] === TokenType.Function) depth++;
+        if (tokens[close][0] === TokenType.CloseParen) depth--;
+      }
+      if (depth) continue;
+      const content = tokens
+        .slice(i + 1, close - 1)
+        .filter((token) => token[0] !== TokenType.Whitespace);
+      if (content.length !== 1 || content[0][0] !== TokenType.String) continue;
+      value = content[0][1].slice(1, -1);
+      quote = content[0][1][0];
+      prefix = functionToken[1];
     }
-    let { value, quote } = /** @type {valueParser.StringNode} */ (
-      node.nodes[0]
-    );
-
-    let optimizedValue;
 
     try {
-      if (dataURIBase64.test(value)) {
-        const url = new URL(value);
-        const base64String = `${url.protocol}${url.pathname}`.replace(
-          dataURI,
-          ''
-        );
-        const svg = Buffer.from(base64String, 'base64').toString('utf8');
-        const { result } = minifySVG(svg, opts);
-        const data = Buffer.from(result).toString('base64');
-        optimizedValue = 'data:image/svg+xml;base64,' + data + url.hash;
-      } else if (dataURI.test(value)) {
-        const svg = value.replace(dataURI, '');
-        const { result, isUriEncoded } = minifySVG(svg, opts);
-        let data = isUriEncoded ? encode(result) : result;
-        // Should always encode # otherwise we yield a broken SVG
-        // in Firefox (works in Chrome however). See this issue:
-        // https://github.com/cssnano/cssnano/issues/245
-        data = data.replace(/#/g, '%23');
-        optimizedValue = 'data:image/svg+xml;charset=utf-8,' + data;
-        quote = isUriEncoded ? '"' : "'";
-      } else {
-        return;
-      }
+      const optimized = optimizeDataUri(value, opts);
+      if (!optimized) return;
+      value = optimized.value;
+      quote = optimized.quote || quote;
     } catch (error) {
       decl.warn(postcssResult, `${error}`);
       return;
     }
-    node.nodes[0] = Object.assign({}, node.nodes[0], {
-      value: optimizedValue,
-      quote: quote,
-      type: 'string',
-      before: '',
-      after: '',
-    });
-
-    return false;
-  });
-
-  decl.value = minified.toString();
+    replacements.push([
+      functionToken[2],
+      tokens[close - 1][3] + 1,
+      prefix + quote + value + quote + ')',
+    ]);
+    i = close - 1;
+  }
+  let result = original;
+  for (const [a, b, text] of replacements.toReversed())
+    result = result.slice(0, a) + text + result.slice(b);
+  decl.value = result;
 }
 /** @typedef {{encode?: boolean} & import('svgo').Config} Options */
 /**
