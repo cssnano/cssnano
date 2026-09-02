34f035de629979623b6ec938771ff2dc8df5a9d6
Ludovico Fischer <livrerie@gmail.com>
2026-09-02 11:20:09 +0200

    refactor(cssnano-utils): replace parser helpers with tokenizer utilities

--- packages/cssnano-utils/src/index.js
+++ packages/cssnano-utils/src/index.js
@@ -1,8 +1,31 @@
 import rawCache from './rawCache.js';
-import getArguments from './getArguments.js';
 import sameParent from './sameParent.js';
+import {
+  TokenType,
+  applyEdits,
+  balancedTokens,
+  decoded,
+  numeric,
+  numericSource,
+  tokenEnd,
+  tokenStart,
+  tokens,
+} from './value.js';
 
-const cssnanoUtils = { rawCache, getArguments, sameParent };
+/** @type {{rawCache: typeof rawCache, sameParent: typeof sameParent, TokenType: typeof TokenType, applyEdits: typeof applyEdits, balancedTokens: typeof balancedTokens, decoded: typeof decoded, numeric: typeof numeric, numericSource: typeof numericSource, tokenEnd: typeof tokenEnd, tokenStart: typeof tokenStart, tokens: typeof tokens}} */
+const cssnanoUtils = {
+  rawCache,
+  sameParent,
+  TokenType,
+  decoded,
+  numeric,
+  applyEdits,
+  balancedTokens,
+  numericSource,
+  tokenEnd,
+  tokenStart,
+  tokens,
+};
 
 const moduleExports = cssnanoUtils;
 
