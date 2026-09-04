11912e25021961a47a961c471123ce1460c7f777
Ludovico Fischer <livrerie@gmail.com>
2026-09-04 11:33:23 +0200

    fix(postcss-minify-selectors): support more pseudo classes

--- packages/postcss-minify-selectors/types/lib/grammar.d.ts
+++ packages/postcss-minify-selectors/types/lib/grammar.d.ts
@@ -6,14 +6,17 @@
  *   | 'an-plus-b-of'
  *   | 'an-plus-b'
  *   | 'ident-or-string-list'
+ *   | 'ident-list'
  *   | 'ident'
+ *   | 'pt-name-selector'
  * } ArgumentGrammar
  */
-export type ArgumentGrammar = 'forgiving-selector-list' | 'selector-list' | 'relative-selector-list' | 'compound-selector' | 'an-plus-b-of' | 'an-plus-b' | 'ident-or-string-list' | 'ident';
+export type ArgumentGrammar = 'forgiving-selector-list' | 'selector-list' | 'relative-selector-list' | 'compound-selector' | 'an-plus-b-of' | 'an-plus-b' | 'ident-or-string-list' | 'ident-list' | 'ident' | 'pt-name-selector';
 export type GrammarEntry = ArgumentGrammar;
 /** @typedef {ArgumentGrammar} GrammarEntry */
 export declare const legacyPseudoElements: Set<string>;
 export declare const pseudoElements: Set<string>;
+export declare const functionalPseudoElements: Set<string>;
 export declare const operators: Set<string>;
 export declare const safePseudos: Set<string>;
 /**
