f2a394f527a1fc99295ec6a59f5959a445d86f7d
Ludovico Fischer <livrerie@gmail.com>
2026-09-07 09:43:49 +0200

    perf(postcss-minify-selectors): skip non-minifiable selectors

---
+++ packages/postcss-minify-selectors/types/lib/fixedPointSelector.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Classifies a narrow set of selector spellings that normalization cannot
+ * change. This is not a CSS validity or canonical-syntax parser.
+ *
+ * @param {string} source
+ */
+export declare function isFixedPointSelector(source: string): boolean;
+//# sourceMappingURL=fixedPointSelector.d.ts.map
